811f81b093c5cbf18beeb19535d0c9c073206c6a
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2021-02-17 04:58:21 +0900

    ASTノードに関連するオブジェクト追加

---
+++ KSPCompiler/Sources/Domain/Ast/Node/IConstable.cs
@@ -0,0 +1,13 @@
+﻿namespace KSPCompiler.Domain.Ast.Node
+{
+    /// <summary>
+    /// Declaring a constant expressible node.
+    /// </summary>
+    public interface IConstable
+    {
+        /// <summary>
+        /// Returns whether the node is a constant expressible node or not.
+        /// </summary>
+        public bool IsConstant { get; set; }
+    }
+}
