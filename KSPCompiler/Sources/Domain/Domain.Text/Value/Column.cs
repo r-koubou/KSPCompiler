d7b5d6b1ae49a5238de701227f97d4e762bb7eb3
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2021-02-22 05:03:37 +0900

    暗黙の型変換へ変更

--- KSPCompiler/Sources/Domain/Domain.Text/Value/Column.cs
+++ KSPCompiler/Sources/Domain/Domain.Text/Value/Column.cs
@@ -2,6 +2,6 @@ using ValueObjectGenerator;
 
 namespace KSPCompiler.Domain.TextFile.Value
 {
-    [ValueObject( typeof(int), Option = ValueOption.NonValidating )]
+    [ValueObject( typeof(int), Option = ValueOption.Implicit | ValueOption.NonValidating )]
     public partial struct Column {}
 }
--- KSPCompiler/Sources/Domain/Domain.Text/Value/LineNumber.cs
+++ KSPCompiler/Sources/Domain/Domain.Text/Value/LineNumber.cs
@@ -2,7 +2,7 @@ using ValueObjectGenerator;
 
 namespace KSPCompiler.Domain.TextFile.Value
 {
-    [ValueObject( typeof(int), Option = ValueOption.NonValidating)]
+    [ValueObject( typeof(int), Option = ValueOption.Implicit | ValueOption.NonValidating)]
     [NotNegative]
     public partial struct LineNumber {}
 }
