eb2e33d4a2b13268e1db1b997f31261064577aea
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2022-08-06 21:44:57 +0900

    シンボル収集ユースケース作成

---
+++ Compiler/UseCases/CorrectSymbolUseCase.cs
@@ -0,0 +1,15 @@
+namespace KSPCompiler.UseCases
+{
+    public interface ICorrectSymbolUseCase
+    {
+        CorrectSymbolResponse Execute( CorrectSymbolRequest request );
+    }
+
+    public class CorrectSymbolRequest
+    {
+    }
+
+    public class CorrectSymbolResponse
+    {
+    }
+}
