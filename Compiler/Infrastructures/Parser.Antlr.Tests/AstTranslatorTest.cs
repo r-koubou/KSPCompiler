dbe173e418d9bd619d7b3478c1daec1a31626946
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2023-09-28 22:27:20 +0900

    テストコードに使うKSPスクリプトをハードコードからファイルに移動させた

--- Compiler/Infrastructures/Parser.Antlr.Tests/AstTranslatorTest.cs
+++ Compiler/Infrastructures/Parser.Antlr.Tests/AstTranslatorTest.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Text;
 
 using KSPCompiler.Domain;
@@ -12,90 +13,55 @@ namespace KSPCompiler.Parser.Antlr.Tests;
 [TestFixture]
 public class AstTranslatorTest
 {
+    private static readonly string testDataDirectory = Path.Combine( "TestData", "AstTranslatorTest" );
+
     // ReSharper disable once UnusedMethodReturnValue.Local
-    private static AstCompilationUnit TranslateImpl( string script )
+    private static AstCompilationUnit TranslateImpl( string scriptFilePath )
     {
-        var p = new KspStringSyntaxAnalyser( script, ICompilerMessageManger.CreateDefault(), Encoding.UTF8 );
+        var path = Path.Combine(
+            TestContext.CurrentContext.TestDirectory,
+            testDataDirectory,
+            scriptFilePath
+        );
+
+        var p = new KspFileSyntaxAnalyser( path, ICompilerMessageManger.CreateDefault(), Encoding.UTF8 );
         return p.Analyse();
     }
 
     [Test]
     public void LexerErrorTest()
     {
-        const string script = @"
-@@ @ @ - - - - on hoge( $arg1, $arg2,
-end on
-";
-        Assert.Throws<KspScriptParseException>( () => TranslateImpl( script ) );
+        Assert.Throws<KspScriptParseException>( () => TranslateImpl( "LexerErrorTest.txt" ) );
     }
 
     [Test]
     public void ParserErrorTest()
     {
-        const string script = @"
-on hoge( $arg1, $arg2,
-end on
-";
-        Assert.Throws<KspScriptParseException>( () => TranslateImpl( script ) );
+        Assert.Throws<KspScriptParseException>( () => TranslateImpl( "ParserErrorTest.txt" ) );
     }
 
     [Test]
     public void ExpressionTest()
     {
-        const string script = @"
-on hoge( $arg1, $arg2, $arg3 )
-    $i := 0 + 1 + 2 + 91234h
-    declare $i[100] (1,2,3,4)
-    declare $j := 100
-    declare ui_table $z(1,2,3)
-end on
-";
-        TranslateImpl( script );
+        Assert.DoesNotThrow( () => TranslateImpl( "ExpressionTest.txt" ) );
     }
 
     [Test]
     public void CallCommandTest()
     {
-        const string script = @"
-on hoge( $arg1, $arg2, $arg3 )
-    command( -0 )
-end on
-";
-        TranslateImpl( script );
+        Assert.DoesNotThrow( () => TranslateImpl( "CallCommandTest.txt" ) );
     }
 
     [Test]
     public void AssignTest()
     {
-        const string script = @"
-on init
-    x := x + 1
-end on
-";
-        TranslateImpl( script );
+        Assert.DoesNotThrow( () => TranslateImpl( "AssignTest.txt" ) );
     }
 
     [Test]
     public void StatementTest()
     {
-        const string script = @"
-on init
-    if( a = 0 )
-        command()
-    end if
-    select( x )
-        case 0
-            command()
-        case 1 to 9
-            command1to9()
-    end select
-    while( x < 10 )
-        x := x + 1
-    end while
-    call userFunc
-end on
-";
-        TranslateImpl( script );
+        Assert.DoesNotThrow( () => TranslateImpl( "StatementTest.txt" ) );
     }
 
 }
