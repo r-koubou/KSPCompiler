4ad42e6df6f864e01523d98514a003751d2e2645
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2023-10-03 00:22:54 +0900

    意味解析とは別のフェーズとして作成していたシンボル収集部分を削除
    意味解析でトラバーススロ機に収集＆チェックが妥当と思い、削除したコードはgistにコピー

--- Compiler/Domain/Analyses/Symbol/SymbolCollectorException.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace KSPCompiler.Domain.Analyses.Symbol;
-
-public class SymbolCollectorException : Exception
-{
-    public SymbolCollectorException( string message ) : base( message ) {}
-}
