108022e64d9a7681edabd5880d4a6e80ce69ce7b
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-05-22 00:00:04 +0900

    Add周りの整理
    - Mergeを廃止、Addでoverwriteの指定を受けとる
    - 実際にテーブルにAddされる直前に OnWillAdd コールバックを実行、各テーブル型のAdd内で実装していたコールバック内でシンボル固有の初期化処理を設定可能にした

--- Compiler/Domain/Symbols/UITypeSymbolTable.cs
+++ Compiler/Domain/Symbols/UITypeSymbolTable.cs
@@ -1,11 +1,3 @@
-using System;
-
 namespace KSPCompiler.Domain.Symbols;
 
-public class UITypeSymbolTable : SymbolTable<UITypeSymbol>
-{
-    public override bool Add( UITypeSymbol symbol )
-    {
-        throw new NotImplementedException();
-    }
-}
+public class UITypeSymbolTable : SymbolTable<UITypeSymbol> {}
