606b524748a6fde2c4b198b73948cd63abc0cd0b
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-05-22 22:19:45 +0900

    TSV入出力の実装

---
+++ Compiler/Infrastructures/ExternalSymbolRepository.Tsv/UITypes/TsvUITypeSymbolImporter.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using KSPCompiler.Commons.Contents;
+using KSPCompiler.Domain.Symbols;
+using KSPCompiler.ExternalSymbolRepository.Tsv.UITypes.Translators;
+
+namespace KSPCompiler.ExternalSymbolRepository.Tsv.UITypes;
+
+public class TsvUITypeSymbolImporter : ISymbolImporter<UITypeSymbol>
+{
+    private readonly ITextContentReader contentReader;
+
+    public TsvUITypeSymbolImporter( ITextContentReader reader )
+    {
+        contentReader = reader;
+    }
+
+    public async Task<IReadOnlyCollection<UITypeSymbol>> ImportAsync( CancellationToken cancellationToken = default )
+    {
+        var tsv = await contentReader.ReadContentAsync( cancellationToken );
+
+        return new FromTsvTranslator().Translate( tsv );
+    }
+}
