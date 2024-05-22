163c82094d431844ed0d2186cc231fa4e4bb1158
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-05-22 21:51:12 +0900

    暫定のメンバ定義

--- Compiler/Domain/Symbols/UITypeSymbol.cs
+++ Compiler/Domain/Symbols/UITypeSymbol.cs
@@ -1,11 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 using KSPCompiler.Domain.Symbols.MetaData;
 
 namespace KSPCompiler.Domain.Symbols;
 
 public sealed class UITypeSymbol : SymbolBase
 {
+    /// <summary>
+    /// Null Object
+    /// </summary>
+    public static readonly UITypeSymbol Null;
+
+    /// <summary>
+    /// Represents any UI type.
+    /// </summary>
+    public static readonly UITypeSymbol AnyUI;
+
+    /// <summary>
+    /// Static constructor
+    /// </summary>
+    static UITypeSymbol()
+    {
+        Null = new UITypeSymbol( false )
+        {
+            Name             = "null",
+            DataType         = DataTypeFlag.None,
+            Reserved = true
+        };
+
+        AnyUI = new UITypeSymbol( false )
+        {
+            Name             = "ui_*",
+            DataType         = DataTypeFlag.MultipleType,
+            Reserved = true
+        };
+    }
+
+    ///
+    /// <inheritdoc/>
+    ///
     public override SymbolType Type
         => SymbolType.UI;
+
+    private readonly List<VariableSymbol> arguments = new ();
+
+    public IReadOnlyList<VariableSymbol> InitializerArguments
+        => arguments;
+
+    /// <summary>
+    /// True if the UI type requires an initializer.
+    /// </summary>
+    public bool InitializerRequired
+        => arguments.Any();
+
+    /// <summary>
+    /// Ctor
+    /// </summary>
+    public UITypeSymbol( bool initializerRequired, IEnumerable<VariableSymbol> initializerArguments )
+    {
+        arguments.AddRange( initializerArguments );
+        DataTypeModifier = DataTypeModifierFlag.UI;
+    }
+
+    /// <summary>
+    /// Ctor
+    /// </summary>
+    public UITypeSymbol( bool initializerRequired ) : this( initializerRequired, new List<VariableSymbol>() ) {}
+
+    public void AddInitializerArgument( VariableSymbol arg )
+    {
+        if( arguments.Contains( arg ))
+        {
+            throw new InvalidOperationException( $"Initializer argument {arg.Name} already exists in {Name}" );
+        }
+        arguments.Add( arg );
+    }
 
     // TODO Implementation
 }
