c64dd46d8d51e5096b09018a3ac56b406e435866
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-06-05 16:38:37 +0900

    WIP TSV入出力

---
+++ Compiler/Infrastructures/ExternalSymbolRepository.Tsv/Callbacks/Translators/ToTsvTranslator.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Text;
+
+using KSPCompiler.Commons;
+using KSPCompiler.Domain.Symbols;
+using KSPCompiler.Domain.Symbols.MetaData;
+using KSPCompiler.ExternalSymbolRepository.Tsv.Extensions;
+
+namespace KSPCompiler.ExternalSymbolRepository.Tsv.Callbacks.Translators;
+
+internal class ToTsvTranslator : IDataTranslator<IEnumerable<CallbackSymbol>, string>
+{
+    public string Translate( IEnumerable<CallbackSymbol> source )
+    {
+        var result = new StringBuilder();
+
+        foreach( var v in source )
+        {
+            result.AppendTab( v.Name )
+                  .AppendTab( v.Reserved.ToString().ToLower() )
+                  .AppendTab( v.Description )
+                  .Append( v.AllowMultipleDeclarations.ToString().ToLower() );
+
+            if( v.Arguments.Count == 0 )
+            {
+                result.AppendTab();
+                continue;
+            }
+
+            result.AppendTab();
+
+            foreach( var x in v.Arguments )
+            {
+                result.AppendTab( DataTypeUtility.ToString( x.DataType ), x.Name )
+                      .AppendTab( x.RequiredDeclareOnInit.ToString().ToLower() )
+                      .AppendTab( x.Description );
+            }
+
+            result.AppendTab();
+        }
+
+        return result.ToString();
+    }
+}
