dd31eae2ea26d031ac47f7d0090abc02822f6726
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-06-11 22:49:34 +0900

    暫定の骨組み作成
    TODO テストコードを書きながらレッドからグリーンにしていく

--- Compiler/Domain/Ast/Analyzers/SymbolAnalyzer.cs
+++ Compiler/Domain/Ast/Analyzers/SymbolAnalyzer.cs
@@ -1,18 +1,50 @@
-using System;
-
+using KSPCompiler.Domain.Ast.Node;
 using KSPCompiler.Domain.Ast.Node.Blocks;
+using KSPCompiler.Domain.CompilerMessages;
 using KSPCompiler.Domain.Symbols;
 
 namespace KSPCompiler.Domain.Ast.Analyzers;
 
-public class SymbolAnalyzer : ISymbolAnalyzer
+public class SymbolAnalyzer : AstVisitorAdaptor<AstNode>, ISymbolAnalyzer
 {
+    private ICompilerMessageManger CompilerMessageManger { get; }
+
     #region Symbol Tables
     public ISymbolTable<VariableSymbol> Variables { get; } = new VariableSymbolTable();
     #endregion
 
+    public SymbolAnalyzer( ICompilerMessageManger compilerMessageManger )
+    {
+        CompilerMessageManger = compilerMessageManger;
+    }
+
     public void Analyze( AstCompilationUnit node )
     {
-        throw new NotImplementedException();
+        node.Accept( this );
     }
+
+    #region IAstVisitor
+    public override AstNode Visit( AstCallbackDeclaration node )
+    {
+        return node.Name == "init"
+            // init コールバックでのみ変数宣言が可能
+            ? AnalyzeInitCallback( node )
+            // init 以外のコールバックでは変数宣言はできない
+            // 変数宣言を見つけ次第エラー
+            : AnalyzeNonInitCallback( node );
+    }
+
+    private AstNode AnalyzeNonInitCallback( AstCallbackDeclaration node )
+    {
+        // TODO 解析、変数宣言を見つけたらエラー扱いにする
+        return node.Accept( this );
+    }
+
+    private AstNode AnalyzeInitCallback( AstCallbackDeclaration node )
+    {
+        // TODO 解析、変数宣言を見つけたら変数テーブルへ追加
+        return node.Accept( this );
+    }
+
+    #endregion
 }
