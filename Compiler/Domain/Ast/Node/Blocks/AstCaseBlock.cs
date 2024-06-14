d93224bd55fea87f08ef911cbbc69f58fc91763f
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-06-14 16:20:51 +0900

    他のクラスと同様のプロパティの初期化場所の統一

--- Compiler/Domain/Ast/Node/Blocks/AstCaseBlock.cs
+++ Compiler/Domain/Ast/Node/Blocks/AstCaseBlock.cs
@@ -8,12 +8,12 @@ namespace KSPCompiler.Domain.Ast.Node.Blocks
         /// <summary>
         /// Conditional expression (starting value)
         /// </summary>
-        public AstExpressionSyntaxNode ConditionFrom { get; set; } = NullAstExpressionSyntaxNode.Instance;
+        public AstExpressionSyntaxNode ConditionFrom { get; set; }
 
         /// <summary>
         /// Conditional expression (end value *optional)
         /// </summary>
-        public AstExpressionSyntaxNode ConditionTo { get; set; } = NullAstExpressionSyntaxNode.Instance;
+        public AstExpressionSyntaxNode ConditionTo { get; set; }
 
         /// <summary>
         /// Code block to be executed when the case condition is matched
@@ -24,17 +24,23 @@ namespace KSPCompiler.Domain.Ast.Node.Blocks
         /// Ctor
         /// </summary>
         public AstCaseBlock()
-            : this( NullAstNode.Instance )
-        {
-        }
+            : this( NullAstNode.Instance ) {}
 
         /// <summary>
         /// Ctor
         /// </summary>
         public AstCaseBlock( IAstNode parent )
+            : this( parent, NullAstExpressionSyntaxNode.Instance, NullAstExpressionSyntaxNode.Instance, new AstBlock() ) {}
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        public AstCaseBlock( IAstNode parent, AstExpressionSyntaxNode conditionFrom, AstExpressionSyntaxNode conditionTo, AstBlock codeBlock )
             : base( AstNodeId.CaseBlock, parent )
         {
-            CodeBlock = new AstBlock( this );
+            ConditionFrom = conditionFrom;
+            ConditionTo   = conditionTo;
+            CodeBlock     = codeBlock;
         }
 
         #region IAstNodeAcceptor
