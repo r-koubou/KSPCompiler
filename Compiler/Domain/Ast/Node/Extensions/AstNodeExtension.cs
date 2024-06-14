3a6f1ed0b01887c3762ee487a9e7750c329d9bb3
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-06-14 15:34:21 +0900

    NullObject判定用の拡張メソッド作成

---
+++ Compiler/Domain/Ast/Node/Extensions/AstNodeExtension.cs
@@ -0,0 +1,20 @@
+using KSPCompiler.Domain.Ast.Node.Statements;
+
+namespace KSPCompiler.Domain.Ast.Node.Extensions;
+
+public static class AstNodeExtension
+{
+    /// <summary>
+    /// Check if the node is Null Object
+    /// </summary>
+    /// <param name="node">A node instance</param>
+    /// <returns>true if the node is Null Object otherwise false.</returns>
+    /// <seealso cref="NullAstNode"/>
+    /// <seealso cref="NullAstExpressionSyntaxNode"/>
+    /// <seealso cref="NullAstExpressionSyntaxNode"/>
+    public static bool IsNull( this IAstNode? node )
+        => node is null
+            or NullAstNode
+            or NullAstExpressionSyntaxNode
+            or NullAstInitializer;
+}
