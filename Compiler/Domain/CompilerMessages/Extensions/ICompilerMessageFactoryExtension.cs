fed1408aee6f458b23d9b5afb7de282bae148716
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-06-17 22:11:29 +0900

    書式付きメッセージのデータに指定可能なFormat版の追加

--- Compiler/Domain/CompilerMessages/Extensions/ICompilerMessageFactoryExtension.cs
+++ Compiler/Domain/CompilerMessages/Extensions/ICompilerMessageFactoryExtension.cs
@@ -8,6 +8,7 @@ namespace KSPCompiler.Domain.CompilerMessages.Extensions;
 // ReSharper disable once InconsistentNaming
 public static class ICompilerMessageFactoryExtension
 {
+    #region Alias
     /// <summary>
     /// Alias for <see cref="ICompilerMessageFactory.Create(CompilerMessageLevel, string, int, int, Exception)"/>.
     /// </summary>
@@ -37,4 +38,62 @@ public static class ICompilerMessageFactoryExtension
     /// </summary>
     public static CompilerMessage Fatal( this ICompilerMessageFactory self, string message )
         => self.Create( CompilerMessageLevel.Fatal, message );
+
+    #endregion ~Alias
+
+
+    #region Alias and Format Support
+
+    /// <summary>
+    /// Alias for <see cref="ICompilerMessageFactory.Create(CompilerMessageLevel, string, int, int, Exception)"/>.
+    /// </summary>
+    /// <remarks>
+    /// Format support version of <see cref="Create(KSPCompiler.Domain.CompilerMessages.ICompilerMessageFactory,KSPCompiler.Domain.CompilerMessages.CompilerMessageLevel,string)"/>.
+    /// </remarks>
+    /// <seealso cref="string.Format(System.IFormatProvider,string,object)"/>
+    public static CompilerMessage Create( this ICompilerMessageFactory self, CompilerMessageLevel level, string message, params object[] parameters )
+        => self.Create( level, string.Format( message, parameters ) );
+
+    /// <summary>
+    /// Alias for <see cref="ICompilerMessageFactory.Create(CompilerMessageLevel, string, int, int, Exception)"/> with <see cref="CompilerMessageLevel.Info"/>.
+    /// </summary>
+    /// <remarks>
+    /// Format support version of <see cref="Info(KSPCompiler.Domain.CompilerMessages.ICompilerMessageFactory,string)"/>
+    /// </remarks>
+    /// <seealso cref="string.Format(System.IFormatProvider,string,object)"/>
+    public static CompilerMessage Info( this ICompilerMessageFactory self, string message, params object[] parameters )
+        => self.Create( CompilerMessageLevel.Info, string.Format( message, parameters ) );
+
+    /// <summary>
+    /// Alias for <see cref="ICompilerMessageFactory.Create(CompilerMessageLevel, string, int, int, Exception)"/> with <see cref="CompilerMessageLevel.Warning"/>.
+    /// </summary>
+    /// <remarks>
+    /// Format support version of <see cref="Info(KSPCompiler.Domain.CompilerMessages.ICompilerMessageFactory,string)"/>
+    /// </remarks>
+    /// <seealso cref="string.Format(System.IFormatProvider,string,object)"/>
+    public static CompilerMessage Warning( this ICompilerMessageFactory self, string message, params object[] parameters )
+        => self.Create( CompilerMessageLevel.Warning, string.Format( message, parameters ) );
+
+    /// <summary>
+    /// Alias for <see cref="ICompilerMessageFactory.Create(CompilerMessageLevel, string, int, int, Exception)"/> with <see cref="CompilerMessageLevel.Error"/>.
+    /// </summary>
+    /// <remarks>
+    /// Format support version of <see cref="Info(KSPCompiler.Domain.CompilerMessages.ICompilerMessageFactory,string)"/>
+    /// </remarks>
+    /// <seealso cref="string.Format(System.IFormatProvider,string,object)"/>
+    public static CompilerMessage Error( this ICompilerMessageFactory self, string message, params object[] parameters )
+        => self.Create( CompilerMessageLevel.Error, string.Format( message, parameters ) );
+
+    /// <summary>
+    /// Alias for <see cref="ICompilerMessageFactory.Create(CompilerMessageLevel, string, int, int, Exception)"/> with <see cref="CompilerMessageLevel.Fatal"/>.
+    /// </summary>
+    /// <remarks>
+    /// Format support version of <see cref="Info(KSPCompiler.Domain.CompilerMessages.ICompilerMessageFactory,string)"/>
+    /// </remarks>
+    /// <seealso cref="string.Format(System.IFormatProvider,string,object)"/>
+    public static CompilerMessage Fatal( this ICompilerMessageFactory self, string message, params object[] parameters )
+        => self.Create( CompilerMessageLevel.Fatal, string.Format( message, parameters ) );
+
+    #endregion
+
 }
--- Compiler/Domain/CompilerMessages/Extensions/ICompilerMessageMangerExtension.cs
+++ Compiler/Domain/CompilerMessages/Extensions/ICompilerMessageMangerExtension.cs
@@ -6,6 +6,8 @@ namespace KSPCompiler.Domain.CompilerMessages.Extensions;
 // ReSharper disable once InconsistentNaming
 public static class ICompilerMessageMangerExtension
 {
+    #region Alias
+
     /// <summary>
     /// Alias for <see cref="ICompilerMessageManger.Append(CompilerMessage)"/>.
     /// </summary>
@@ -35,4 +37,60 @@ public static class ICompilerMessageMangerExtension
     /// </summary>
     public static void Fatal( this ICompilerMessageManger self, string message )
         => self.Append( self.MessageFactory.Fatal( message ) );
+
+    #endregion ~Alias
+
+    #region Alias and Format Support
+
+    /// <summary>
+    /// Alias for <see cref="ICompilerMessageManger.Append(CompilerMessage)"/>.
+    /// </summary>
+    /// <remarks>
+    /// Format support version of <see cref="Append(KSPCompiler.Domain.CompilerMessages.ICompilerMessageManger,KSPCompiler.Domain.CompilerMessages.CompilerMessageLevel,string)" />.
+    /// </remarks>
+    /// <seealso cref="string.Format(System.IFormatProvider,string,object)"/>
+    public static void Append( this ICompilerMessageManger self, CompilerMessageLevel level, string format, params object[] parameters )
+        => self.Append( self.MessageFactory.Create( level, string.Format( format, parameters ) ) );
+
+    /// <summary>
+    /// Alias for <see cref="ICompilerMessageManger.Append(CompilerMessage)"/> with <see cref="CompilerMessageLevel.Info"/>.
+    /// </summary>
+    /// <remarks>
+    /// Format support version of <see cref="Info(ICompilerMessageManger, string)"/>.
+    /// </remarks>
+    /// <seealso cref="string.Format(System.IFormatProvider,string,object)"/>
+    public static void Info( this ICompilerMessageManger self, string format, params object[] parameters )
+        => self.Append( self.MessageFactory.Info( string.Format( format, parameters ) ) );
+
+    /// <summary>
+    /// Alias for <see cref="ICompilerMessageManger.Append(CompilerMessage)"/> with <see cref="CompilerMessageLevel.Warning"/>.
+    /// </summary>
+    /// <remarks>
+    /// Format support version of <see cref="Warning(ICompilerMessageManger, string)"/>.
+    /// </remarks>
+    /// <seealso cref="string.Format(System.IFormatProvider,string,object)"/>
+    public static void Warning( this ICompilerMessageManger self, string format, params object[] parameters )
+        => self.Append( self.MessageFactory.Warning( string.Format( format, parameters ) ) );
+
+    /// <summary>
+    /// Alias for <see cref="ICompilerMessageManger.Append(CompilerMessage)"/> with <see cref="CompilerMessageLevel.Error"/>.
+    /// </summary>
+    /// <remarks>
+    /// Format support version of <see cref="Error(ICompilerMessageManger, string)"/>.
+    /// </remarks>
+    /// <seealso cref="string.Format(System.IFormatProvider,string,object)"/>
+    public static void Error( this ICompilerMessageManger self, string format, params object[] parameters )
+        => self.Append( self.MessageFactory.Error( string.Format( format, parameters ) ) );
+
+    /// <summary>
+    /// Alias for <see cref="ICompilerMessageManger.Append(CompilerMessage)"/> with <see cref="CompilerMessageLevel.Fatal"/>.
+    /// </summary>
+    /// <remarks>
+    /// Format support version of <see cref="Fatal(ICompilerMessageManger, string)"/>.
+    /// </remarks>
+    /// <seealso cref="string.Format(System.IFormatProvider,string,object)"/>
+    public static void Fatal( this ICompilerMessageManger self, string format, params object[] parameters )
+        => self.Append( self.MessageFactory.Fatal( string.Format( format, parameters ) ) );
+
+    #endregion ~Alias and Format Support
 }
