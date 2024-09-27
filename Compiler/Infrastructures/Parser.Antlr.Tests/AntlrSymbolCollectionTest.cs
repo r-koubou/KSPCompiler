b49da8df023292a79bb92936de687297edcf4405
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-09-27 22:29:42 +0900

    ブランチのスコープ外だったため一時的にコードを退避
    コールバック収集用の作業ブランチ作成後再開

--- Compiler/Infrastructures/Parser.Antlr.Tests/AntlrSymbolCollectionTest.cs
+++ Compiler/Infrastructures/Parser.Antlr.Tests/AntlrSymbolCollectionTest.cs
@@ -24,17 +24,4 @@ public class AntlrSymbolCollectionTest
         compilerMessageManger.WriteTo( System.Console.Out );
         Assert.IsTrue( symbolAnalyzer.Variables.Count == 1 );
     }
-
-    [Test]
-    public void CallbackSymbolAnalyzeTest()
-    {
-        var compilerMessageManger = ICompilerMessageManger.Default;
-        var ast = ParseTestUtility.Parse( TestDataDirectory, "CallbackSymbolTest.txt" );
-        var symbolAnalyzer = new SymbolCollector( compilerMessageManger );
-
-        Assert.DoesNotThrow( () => { symbolAnalyzer.Analyze( ast ); } );
-        compilerMessageManger.WriteTo( System.Console.Out );
-        Assert.IsTrue( symbolAnalyzer.Callbacks.Count == 1 );
-
-    }
 }
