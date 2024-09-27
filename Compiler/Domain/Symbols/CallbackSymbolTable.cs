dbce8f3ff50b80158eca7587f53a717ac1c674b7
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-09-27 22:53:24 +0900

    ブランチのスコープ外だったため一時的にコードを退避
    コールバック収集用の作業ブランチ作成後再開

--- Compiler/Domain/Symbols/CallbackSymbolTable.cs
+++ Compiler/Domain/Symbols/CallbackSymbolTable.cs
@@ -1,28 +1,3 @@
 namespace KSPCompiler.Domain.Symbols;
 
-public class CallbackSymbolTable : SymbolTable<CallbackSymbol>
-{
-    public override bool Add( CallbackSymbol symbol, bool overwrite = false )
-    {
-        var contains = table.ContainsKey( symbol.Name );
-
-        // 既に登録済みの場合
-        if( contains && !overwrite)
-        {
-            // 多重定義を許可されていないコールバックの場合は追加失敗扱い
-            if( !symbol.AllowMultipleDeclaration )
-            {
-                return false;
-            }
-            // そうでない場合は追加済みとして扱う
-            OnWillAdd( symbol );
-            return true;
-        }
-
-        // 新規登録
-        OnWillAdd( symbol );
-        symbol.TableIndex    = uniqueIndexGenerator.Next();
-        table[ symbol.Name ] = symbol;
-        return true;
-    }
-}
+public class CallbackSymbolTable : SymbolTable<CallbackSymbol> {}
