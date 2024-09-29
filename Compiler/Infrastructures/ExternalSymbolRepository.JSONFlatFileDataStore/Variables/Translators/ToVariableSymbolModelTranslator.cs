8c0b6b553dc150020f833011e73a229c53849bfa
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-09-29 22:21:52 +0900

    Variableのリポジトリ作成

---
+++ Compiler/Infrastructures/ExternalSymbolRepository.JSONFlatFileDataStore/Variables/Translators/ToVariableSymbolModelTranslator.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+
+using KSPCompiler.Commons;
+using KSPCompiler.Domain.Symbols;
+using KSPCompiler.ExternalSymbolRepository.JSONFlatFileDataStore.Variables.Models;
+
+namespace KSPCompiler.ExternalSymbolRepository.JSONFlatFileDataStore.Variables.Translators;
+
+internal class ToVariableSymbolModelTranslator : IDataTranslator<IEnumerable<VariableSymbol>, IReadOnlyCollection<VariableSymbolModel>>
+{
+    public IReadOnlyCollection<VariableSymbolModel> Translate( IEnumerable<VariableSymbol> source )
+    {
+        var result = new List<VariableSymbolModel>();
+
+        foreach( var x in source )
+        {
+            var symbol = new VariableSymbolModel
+            {
+                Name        = x.Name.Value,
+                Reserved    = x.Reserved,
+                Description = x.Description.Value
+            };
+
+            result.Add( symbol );
+        }
+
+        return result;
+    }
+}
