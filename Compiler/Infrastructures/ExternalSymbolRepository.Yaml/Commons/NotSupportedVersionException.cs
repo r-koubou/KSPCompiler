dbc4b5e6033d873ea908937b3e25a3744fe4c77d
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-10-05 17:19:01 +0900

    不要となったYaml実装を削除

--- Compiler/Infrastructures/ExternalSymbolRepository.Yaml/Commons/NotSupportedVersionException.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace KSPCompiler.ExternalSymbolRepository.Yaml.Commons;
-
-public sealed class NotSupportedVersionException : Exception
-{
-    public NotSupportedVersionException( int version, int expectedVersion )
-        : base( $"Unsupported version: {version}, expected: {expectedVersion}" ) {}
-}
