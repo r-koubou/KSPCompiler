5bfe236b2eda96fd176040f52f4dcdcaac9f6c7f
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-10-06 20:24:02 +0900

    NIのビルトインかとうかを判別するReservedフラグ追記

--- Compiler/Domain/Ast/Nodes/Expressions/AstSymbolExpression.cs
+++ Compiler/Domain/Ast/Nodes/Expressions/AstSymbolExpression.cs
@@ -5,6 +5,11 @@ namespace KSPCompiler.Domain.Ast.Nodes.Expressions
     /// </summary>
     public class AstSymbolExpression : AstExpressionSyntaxNode, INameable
     {
+        /// <summary>
+        /// Represents whether the node is symbol(variable, commands, callback, etc.) and its reserved (built-in) by NI.
+        /// </summary>
+        public virtual bool Reserved { get; set; }
+
         #region INameable
 
         ///
@@ -24,9 +29,13 @@ namespace KSPCompiler.Domain.Ast.Nodes.Expressions
         /// Ctor
         /// </summary>
         public AstSymbolExpression( IAstNode parent )
-            : base( AstNodeId.Symbol, parent )
-        {
-        }
+            : base( AstNodeId.Symbol, parent ) {}
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        public AstSymbolExpression( AstNodeId id, IAstNode parent )
+            : base( id, parent ) {}
 
         #region IAstNodeAcceptor
 
