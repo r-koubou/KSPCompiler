ea97699524f78a5d6114d932417371951c37aa47
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-10-10 23:46:05 +0900

    拡張機能で実装した定数値取得方法の適用漏れ

--- Compiler/Domain/Ast/Analyzers/Convolutions/IntegerConstantConvolutionCalculator.cs
+++ Compiler/Domain/Ast/Analyzers/Convolutions/IntegerConstantConvolutionCalculator.cs
@@ -5,6 +5,7 @@ using KSPCompiler.Domain.Ast.Nodes;
 using KSPCompiler.Domain.Ast.Nodes.Expressions;
 using KSPCompiler.Domain.CompilerMessages;
 using KSPCompiler.Domain.Symbols;
+using KSPCompiler.Domain.Symbols.Extensions;
 using KSPCompiler.Domain.Symbols.MetaData.Extensions;
 using KSPCompiler.Resources;
 
@@ -53,11 +54,12 @@ public sealed class IntegerConstantConvolutionCalculator : IPrimitiveConstantCon
             variable.Referenced = true;
             variable.State      = VariableState.Loaded;
 
-            if( variable.Value is int value )
+            if( variable.TryGetConstantValue<int>( out var value ) )
             {
                 return value;
             }
 
+            // 予約変数（組み込み変数）は定数値を持たない
             return null;
         }
 
