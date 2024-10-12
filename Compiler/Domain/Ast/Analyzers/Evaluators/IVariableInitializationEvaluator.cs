ff47163c9f33b9ad5a0499d98428d425b53973a2
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-10-12 21:48:56 +0900

    初期化式は後にするため一旦削除

--- Compiler/Domain/Ast/Analyzers/Evaluators/IVariableInitializationEvaluator.cs
+++
@@ -1,6 +0,0 @@
-using KSPCompiler.Domain.Ast.Analyzers.Evaluators.Operators;
-using KSPCompiler.Domain.Ast.Nodes;
-
-namespace KSPCompiler.Domain.Ast.Analyzers.Evaluators;
-
-public interface IVariableInitializationEvaluator : IOperatorEvaluator<IAstNode> {}
