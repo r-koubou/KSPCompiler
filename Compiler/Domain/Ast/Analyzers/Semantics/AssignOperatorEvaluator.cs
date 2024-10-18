b0af25081791ba0faf8cda7ecd277def693f4b65
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-10-18 23:04:40 +0900

    シンボルノード評価で行う変数評価の削除、型判定、暗黙の型判定の修正

--- Compiler/Domain/Ast/Analyzers/Semantics/AssignOperatorEvaluator.cs
+++ Compiler/Domain/Ast/Analyzers/Semantics/AssignOperatorEvaluator.cs
@@ -1,8 +1,8 @@
 using KSPCompiler.Domain.Ast.Analyzers.Evaluators.Operators;
 using KSPCompiler.Domain.Ast.Extensions;
 using KSPCompiler.Domain.Ast.Nodes;
+using KSPCompiler.Domain.Ast.Nodes.Expressions;
 using KSPCompiler.Domain.CompilerMessages;
-using KSPCompiler.Domain.Symbols;
 using KSPCompiler.Domain.Symbols.MetaData;
 using KSPCompiler.Domain.Symbols.MetaData.Extensions;
 using KSPCompiler.Resources;
@@ -11,18 +11,21 @@ namespace KSPCompiler.Domain.Ast.Analyzers.Semantics;
 
 public sealed class AssignOperatorEvaluator : IAssignOperatorEvaluator
 {
-    private IAstVisitor AstVisitor { get; }
     private ICompilerMessageManger CompilerMessageManger { get; }
-    private IVariableSymbolTable VariableSymbolTable { get; }
 
-    public AssignOperatorEvaluator(
-        IAstVisitor astVisitor,
-        ICompilerMessageManger compilerMessageManger,
-        IVariableSymbolTable variableSymbolTable )
+    private static AstExpressionNode CreateEvaluateNode( AstExpressionNode source, DataTypeFlag type )
+        => new AstDefaultExpressionNode( source.Id )
+        {
+            Parent   = source.Parent,
+            Name     = source.Name,
+            Left     = source.Left,
+            Right    = source.Right,
+            TypeFlag = type
+        };
+
+    public AssignOperatorEvaluator( ICompilerMessageManger compilerMessageManger )
     {
-        AstVisitor            = astVisitor;
         CompilerMessageManger = compilerMessageManger;
-        VariableSymbolTable   = variableSymbolTable;
     }
 
     public IAstNode Evaluate( IAstVisitor<IAstNode> visitor, AstExpressionNode expr )
@@ -47,20 +50,8 @@ public sealed class AssignOperatorEvaluator : IAssignOperatorEvaluator
             throw new AstAnalyzeException( expr, "Failed to evaluate right side of binary operator" );
         }
 
-        // 変数の確認
-        if( !VariableSymbolTable.TrySearchByName( evaluatedLeft.Name, out var variable ) )
-        {
-            CompilerMessageManger.Error(
-                expr,
-                CompilerMessageResources.semantic_error_variable_not_declared,
-                evaluatedLeft.Name
-            );
-
-            return NullAstExpressionNode.Instance;
-        }
-
         // 定数には代入できない
-        if( variable.DataTypeModifier.IsConstant() )
+        if( evaluatedLeft.Constant )
         {
             CompilerMessageManger.Error(
                 expr,
@@ -71,41 +62,34 @@ public sealed class AssignOperatorEvaluator : IAssignOperatorEvaluator
             return NullAstExpressionNode.Instance;
         }
 
-        var leftType  = expr.Left.TypeFlag;
+        var leftType  = evaluatedLeft.TypeFlag;
         var rightType = evaluatedRight.TypeFlag;
 
+        // 暗黙の型変換
         // コマンドコールの戻り値が複数の型を持つなどで暗黙の型変換を要する場合
-        // 代入先の変数に合わせる。暗黙の型変換が不可能な場合は、代替としてVOIDを入れる
-        if( ( leftType & rightType ) == 0 )
-        {
-            rightType = DataTypeFlag.TypeVoid;
-        }
-        else
-        {
-            // 暗黙の型変換
-            rightType = leftType;
-        }
-
-        // 配列要素への格納もあるので配列属性ビットはマスクさせている
-        if( leftType.TypeMasked() != rightType.TypeMasked() )
+        // 代入先の変数に合わせる。暗黙の型変換が不可能な場合は、以降の型判定で失敗させる
+        if( ( leftType & rightType ) != 0 )
         {
             // 代入先が文字列型なら暗黙の型変換が可能
-            // 文字列型以外なら型の不一致 or 条件式は代入不可
-            if( !evaluatedLeft.TypeFlag.IsString() || evaluatedRight.TypeFlag.IsBoolean() )
+            if( leftType.IsString() )
             {
-                CompilerMessageManger.Error(
-                    expr,
-                    CompilerMessageResources.semantic_error_assign_type_compatible,
-                    leftType.TypeMasked().ToMessageString(),
-                    evaluatedRight.TypeFlag.TypeMasked().ToMessageString()
-                );
-
-                return NullAstExpressionNode.Instance;
+                rightType = leftType.TypeMasked(); // Mask: 型以外の属性は含まない
             }
         }
 
-        variable.State = VariableState.Loaded;
+        // 型の不一致または条件式の代入は不可
+        if( leftType != rightType || evaluatedRight.TypeFlag.IsBoolean() )
+        {
+            CompilerMessageManger.Error(
+                expr,
+                CompilerMessageResources.semantic_error_assign_type_compatible,
+                leftType.ToMessageString(),
+                rightType.ToMessageString()
+            );
 
-        return evaluatedLeft;
+            return CreateEvaluateNode( evaluatedLeft, evaluatedLeft.TypeFlag );
+        }
+
+        return CreateEvaluateNode( evaluatedLeft, evaluatedLeft.TypeFlag );
     }
 }
