ee6e4dc479bfb7562e4684df5a5878b47b9a686e
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-10-23 15:33:29 +0900

    代入時の型判定をユーティリティ化

--- Compiler/Domain/Ast/Analyzers/Semantics/AssignOperatorEvaluator.cs
+++ Compiler/Domain/Ast/Analyzers/Semantics/AssignOperatorEvaluator.cs
@@ -65,23 +65,7 @@ public sealed class AssignOperatorEvaluator : IAssignOperatorEvaluator
         var leftType  = evaluatedLeft.TypeFlag;
         var rightType = evaluatedRight.TypeFlag;
 
-        // 暗黙の型変換
-        // コマンドコールの戻り値が複数の型を持つなどで暗黙の型変換を要する場合
-        // 代入先の変数に合わせる。暗黙の型変換が不可能な場合は、以降の型判定で失敗させる
-        if( ( leftType & rightType ) != 0 )
-        {
-            // 型情報のマージ
-            rightType |= leftType.TypeMasked(); // Mask: 型以外の属性は含まない
-        }
-
-        // 完全一致
-        if( leftType == rightType )
-        {
-            return CreateEvaluateNode( evaluatedLeft, evaluatedLeft.TypeFlag );
-        }
-
-        // 個別に型チェック
-        if( !IsTypeMatched( leftType, rightType ) )
+        if( !AssigningTypeUtility.IsTypeCompatible( leftType, rightType ) )
         {
             CompilerMessageManger.Error(
                 expr,
@@ -94,35 +78,5 @@ public sealed class AssignOperatorEvaluator : IAssignOperatorEvaluator
         }
 
         return CreateEvaluateNode( evaluatedLeft, evaluatedLeft.TypeFlag );
-    }
-
-    private static bool IsTypeMatched( DataTypeFlag leftType, DataTypeFlag rightType )
-    {
-        var result = true;
-
-        // 代入先：配列型
-        // 代入元：非配列型
-        if( leftType.IsArray() && !rightType.IsArray() )
-        {
-            result = false;
-        }
-        // 代入先：非配列型
-        // 代入元：配列型
-        else if( rightType.IsArray() && !leftType.IsArray() )
-        {
-            result = false;
-        }
-        // 型が一致するビットがない
-        else if( ( leftType.TypeMasked() & rightType.TypeMasked() ) == 0 )
-        {
-            // 文字列型へ integer, real 値の代入は可能（文字列への型変換が発生）
-            // -> 代入先が文字列型以外なら型の不一致 or 条件式は代入不可
-            if( !leftType.IsString() || rightType.IsBoolean() )
-            {
-                result = false;
-            }
-        }
-
-        return result;
     }
 }
