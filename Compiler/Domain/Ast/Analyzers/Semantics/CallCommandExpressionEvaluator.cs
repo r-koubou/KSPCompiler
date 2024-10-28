dd71b2793bcd791f02d21c5242d06e61a68d25a5
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-10-28 19:05:59 +0900

    コマンド評価の大枠・関数作成

--- Compiler/Domain/Ast/Analyzers/Semantics/CallCommandExpressionEvaluator.cs
+++ Compiler/Domain/Ast/Analyzers/Semantics/CallCommandExpressionEvaluator.cs
@@ -1,5 +1,3 @@
-using System;
-
 using KSPCompiler.Domain.Ast.Analyzers.Evaluators.Commands;
 using KSPCompiler.Domain.Ast.Nodes;
 using KSPCompiler.Domain.Ast.Nodes.Expressions;
@@ -10,11 +8,40 @@ namespace KSPCompiler.Domain.Ast.Analyzers.Semantics;
 
 public class CallCommandExpressionEvaluator : ICallCommandExpressionEvaluator
 {
+    private ICompilerMessageManger CompilerMessageManger { get; }
+    private ICommandSymbolTable Commands { get; }
+
     public CallCommandExpressionEvaluator( ICompilerMessageManger compilerMessageManger, ICommandSymbolTable commands )
     {
-        throw new NotImplementedException();
+        CompilerMessageManger = compilerMessageManger;
+        Commands              = commands;
     }
 
     public IAstNode Evaluate( IAstVisitor<IAstNode> visitor, AstCallCommandExpressionNode expr )
-        => throw new NotImplementedException();
+    {
+        if( !TryGetCommandSymbol( visitor, expr, out var commandSymbol ) )
+        {
+            return NullAstExpressionNode.Instance;
+        }
+
+        if( !ValidateCommandArguments( visitor, expr, commandSymbol ) )
+        {
+            return NullAstExpressionNode.Instance;
+        }
+
+        var result = expr.Clone<AstCallCommandExpressionNode>();
+        result.TypeFlag = commandSymbol.DataType;
+
+        return result;
+    }
+
+    private bool TryGetCommandSymbol( IAstVisitor<IAstNode> visitor, AstCallCommandExpressionNode expr, out CommandSymbol result )
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private bool ValidateCommandArguments( IAstVisitor<IAstNode> visitor, AstCallCommandExpressionNode expr, object commandSymbol )
+    {
+        throw new System.NotImplementedException();
+    }
 }
