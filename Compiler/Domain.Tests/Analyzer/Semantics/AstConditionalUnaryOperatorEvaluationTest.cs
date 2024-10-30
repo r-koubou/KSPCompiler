af4d8467e857a28b6d7b35ee54d3bea38d6fa149
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-10-30 18:57:56 +0900

    エラー系のテスト追加

--- Compiler/Domain.Tests/Analyzer/Semantics/AstConditionalUnaryOperatorEvaluationTest.cs
+++ Compiler/Domain.Tests/Analyzer/Semantics/AstConditionalUnaryOperatorEvaluationTest.cs
@@ -28,6 +28,7 @@ public class AstConditionalUnaryOperatorEvaluationTest
             compilerMessageManger
         );
 
+        // not 1 = 1
         var operatorNode = new AstUnaryLogicalNotExpressionNode
         {
             Left  = new AstEqualExpressionNode
@@ -45,6 +46,38 @@ public class AstConditionalUnaryOperatorEvaluationTest
         compilerMessageManger.WriteTo( Console.Out );
 
         Assert.AreEqual( 0, compilerMessageManger.Count( CompilerMessageLevel.Error ) );
+        Assert.IsNotNull( result );
+        Assert.AreEqual( DataTypeFlag.TypeBool, result?.TypeFlag );
+    }
+
+    [Test]
+    public void CannotConditionalUnaryOperatorWithoutBooleanTest()
+    {
+        var compilerMessageManger = ICompilerMessageManger.Default;
+        var visitor = new MockAstConditionalUnaryOperatorVisitor();
+
+        var conditionalBinaryOperatorEvaluator = new ConditionalUnaryOperatorEvaluator(
+            compilerMessageManger
+        );
+
+        // not 1 + 1 <-- `1 + 1` is not conditional operator
+        var operatorNode = new AstUnaryLogicalNotExpressionNode
+        {
+            Left  = new AstAdditionExpressionNode
+            {
+                Left  = new AstIntLiteralNode( 1 ),
+                Right = new AstIntLiteralNode( 1 )
+            }
+        };
+        operatorNode.Left.Parent = operatorNode;
+
+        visitor.Inject( conditionalBinaryOperatorEvaluator );
+
+        var result = visitor.Visit( operatorNode ) as AstExpressionNode;
+
+        compilerMessageManger.WriteTo( Console.Out );
+
+        Assert.AreEqual( 1, compilerMessageManger.Count( CompilerMessageLevel.Error ) );
         Assert.IsNotNull( result );
         Assert.AreEqual( DataTypeFlag.TypeBool, result?.TypeFlag );
     }
