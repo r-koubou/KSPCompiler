dd717b23f2b5a45caca3039edd5292195e76501a
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-10-30 20:11:58 +0900

    テストクラス内で書き留めていた各種クラスを別ファイルに移動

--- Compiler/Domain.Tests/Analyzer/Semantics/AstConditionalUnaryOperatorEvaluationTest.cs
+++ Compiler/Domain.Tests/Analyzer/Semantics/AstConditionalUnaryOperatorEvaluationTest.cs
@@ -1,15 +1,10 @@
 using System;
 
-using KSPCompiler.Domain.Ast.Analyzers;
-using KSPCompiler.Domain.Ast.Analyzers.Evaluators.Operators;
-using KSPCompiler.Domain.Ast.Extensions;
+using KSPCompiler.Domain.Ast.Analyzers.Semantics;
 using KSPCompiler.Domain.Ast.Nodes;
 using KSPCompiler.Domain.Ast.Nodes.Expressions;
-using KSPCompiler.Domain.Ast.Nodes.Extensions;
 using KSPCompiler.Domain.CompilerMessages;
 using KSPCompiler.Domain.Symbols.MetaData;
-using KSPCompiler.Domain.Symbols.MetaData.Extensions;
-using KSPCompiler.Resources;
 
 using NUnit.Framework;
 
@@ -83,75 +78,3 @@ public class AstConditionalUnaryOperatorEvaluationTest
         Assert.AreEqual( DataTypeFlag.TypeBool, result?.TypeFlag );
     }
 }
-
-public class ConditionalUnaryOperatorEvaluator : IUnaryOperatorEvaluator
-{
-    private ICompilerMessageManger CompilerMessageManger { get; }
-
-    private static AstExpressionNode CreateEvaluateNode( AstExpressionNode source, DataTypeFlag type )
-    {
-        var result = source.Clone<AstExpressionNode>();
-        result.TypeFlag = type;
-
-        return result;
-    }
-
-    public ConditionalUnaryOperatorEvaluator( ICompilerMessageManger compilerMessageManger )
-        => CompilerMessageManger = compilerMessageManger;
-
-    public IAstNode Evaluate( IAstVisitor<IAstNode> visitor, AstExpressionNode expr )
-    {
-        /*
-          <<logical unary not operator>>
-                      expr
-                       +
-                       |
-          <<conditional binary operator>>
-                    expr.Left
-        */
-
-        if( expr.ChildNodeCount != 1 || !expr.Id.IsBooleanSupportedUnaryOperator())
-        {
-            throw new AstAnalyzeException( expr, "Expected a unary logical not expression" );
-        }
-
-        if( expr.Left.Accept( visitor ) is not AstExpressionNode evaluatedLeft )
-        {
-            throw new AstAnalyzeException( expr, "Failed to evaluate left side of the unary logical not expression" );
-        }
-
-        if( !evaluatedLeft.TypeFlag.IsBoolean() )
-        {
-            CompilerMessageManger.Error(
-                expr,
-                CompilerMessageResources.semantic_error_unrayoprator_logicalnot_incompatible,
-                evaluatedLeft.TypeFlag.ToMessageString()
-            );
-
-            // 上位のノードで評価を継続させるので代替のノードは生成しない
-        }
-
-        return CreateEvaluateNode( expr, DataTypeFlag.TypeBool );
-    }
-}
-
-#region Working mock classes
-
-public class MockConditionalUnaryOperatorEvaluator : IUnaryOperatorEvaluator
-{
-    public IAstNode Evaluate( IAstVisitor<IAstNode> visitor, AstExpressionNode expr )
-        => throw new NotImplementedException();
-}
-
-public class MockAstConditionalUnaryOperatorVisitor : DefaultAstVisitor
-{
-    private IUnaryOperatorEvaluator ConditionalUnaryOperatorEvaluator { get; set; } = new MockConditionalUnaryOperatorEvaluator();
-
-    public void Inject( IUnaryOperatorEvaluator evaluator )
-        => ConditionalUnaryOperatorEvaluator = evaluator;
-
-    public override IAstNode Visit( AstUnaryLogicalNotExpressionNode node )
-        => ConditionalUnaryOperatorEvaluator.Evaluate( this, node );
-}
-
-#endregion
