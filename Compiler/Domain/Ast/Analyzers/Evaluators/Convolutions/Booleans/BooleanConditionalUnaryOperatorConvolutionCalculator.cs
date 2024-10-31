8061d4f44e3c3bf555f9d0cdef264d8973135c66
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-10-31 21:00:11 +0900

    単項条件演算子の畳み込み計算クラス実装

--- Compiler/Domain/Ast/Analyzers/Evaluators/Convolutions/Booleans/BooleanConditionalUnaryOperatorConvolutionCalculator.cs
+++ Compiler/Domain/Ast/Analyzers/Evaluators/Convolutions/Booleans/BooleanConditionalUnaryOperatorConvolutionCalculator.cs
@@ -1,3 +1,5 @@
+using System;
+
 using KSPCompiler.Domain.Ast.Nodes;
 
 namespace KSPCompiler.Domain.Ast.Analyzers.Evaluators.Convolutions.Booleans;
@@ -7,8 +9,31 @@ namespace KSPCompiler.Domain.Ast.Analyzers.Evaluators.Convolutions.Booleans;
 /// </summary>
 public class BooleanConditionalUnaryOperatorConvolutionCalculator : IBooleanConditionalUnaryOperatorConvolutionCalculator
 {
+    private IPrimitiveConvolutionEvaluator<bool> EvaluatorForRecursive { get; }
+
+    public BooleanConditionalUnaryOperatorConvolutionCalculator( IPrimitiveConvolutionEvaluator<bool> evaluatorForRecursive )
+    {
+        EvaluatorForRecursive = evaluatorForRecursive;
+    }
+
     public bool? Calculate( AstExpressionNode expr )
     {
-        throw new System.NotImplementedException();
+        if( expr.ChildNodeCount != 1 )
+        {
+            throw new ArgumentException( $"Expected 1 child node, but got {expr.ChildNodeCount}. (node: {expr.GetType().Name})" );
+        }
+
+        var convolutedValue = EvaluatorForRecursive.Evaluate( expr.Left, false );
+
+        if( convolutedValue == null )
+        {
+            return null;
+        }
+
+        return expr.Id switch
+        {
+            AstNodeId.UnaryLogicalNot => !convolutedValue,
+            _                         => null
+        };
     }
 }
