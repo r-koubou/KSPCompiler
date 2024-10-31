c624ca78c97f08dabd8b4f910b862adcceb22819
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-10-31 21:15:40 +0900

    単項条件演算子の畳み込みテストコード作成

---
+++ Compiler/Domain.Tests/Analyzer/Semantics/AstConditionalUnaryOperatorConvolutionCalculatorTest.cs
@@ -0,0 +1,33 @@
+using KSPCompiler.Domain.Ast.Analyzers.Evaluators.Convolutions.Booleans;
+using KSPCompiler.Domain.Ast.Nodes.Expressions;
+using KSPCompiler.Domain.Symbols.MetaData;
+
+using NUnit.Framework;
+
+namespace KSPCompiler.Domain.Tests.Analyzer.Semantics;
+
+[TestFixture]
+public class AstConditionalUnaryOperatorConvolutionCalculatorTest
+{
+    [TestCase( true )]
+    [TestCase( false )]
+    public void ConvolutionIntegerConditionalOperatorTest( bool value )
+    {
+        var integerConvolutionEvaluator = new MockBooleanConvolutionEvaluator( value ); // always return `value`
+
+        var calculator = new BooleanConditionalUnaryOperatorConvolutionCalculator(
+            integerConvolutionEvaluator
+        );
+
+        var ast = new AstUnaryLogicalNotExpressionNode
+        {
+            TypeFlag = DataTypeFlag.TypeBool,
+            Left     = new AstBooleanLiteralNode( value )
+        };
+
+        var result = calculator.Calculate( ast );
+
+        Assert.IsNotNull( result );
+        Assert.AreEqual( !value, result );
+    }
+}
