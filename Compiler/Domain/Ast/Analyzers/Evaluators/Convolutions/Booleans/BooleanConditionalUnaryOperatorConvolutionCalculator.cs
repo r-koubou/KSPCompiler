7d3681b7c1b48ae8c10428a7d8d935d879994d57
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-10-31 20:06:55 +0900

    単項条件演算子の畳み込み計算クラス本体のみ作成

---
+++ Compiler/Domain/Ast/Analyzers/Evaluators/Convolutions/Booleans/BooleanConditionalUnaryOperatorConvolutionCalculator.cs
@@ -0,0 +1,14 @@
+using KSPCompiler.Domain.Ast.Nodes;
+
+namespace KSPCompiler.Domain.Ast.Analyzers.Evaluators.Convolutions.Booleans;
+
+/// <summary>
+/// Calculator for convolution operations with conditional unary operators (boolean)
+/// </summary>
+public class BooleanConditionalUnaryOperatorConvolutionCalculator : IBooleanConditionalUnaryOperatorConvolutionCalculator
+{
+    public bool? Calculate( AstExpressionNode expr )
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Compiler/Domain/Ast/Analyzers/Evaluators/Convolutions/Booleans/IBooleanConditionalUnaryOperatorConvolutionCalculator.cs
@@ -0,0 +1,6 @@
+namespace KSPCompiler.Domain.Ast.Analyzers.Evaluators.Convolutions.Booleans;
+
+/// <summary>
+/// Calculator for convolution operations with conditional unary operators (boolean)
+/// </summary>
+public interface IBooleanConditionalUnaryOperatorConvolutionCalculator : IConditionalConvolutionCalculator {}
