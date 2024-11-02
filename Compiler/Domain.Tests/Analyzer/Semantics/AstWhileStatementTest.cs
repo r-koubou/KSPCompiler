ca7b2b9431d5f0f4e8bc9499b63a7677bd181146
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-11-02 11:56:55 +0900

    テストコード内で書き留めていた評価ラスを別ファイルに移動

--- Compiler/Domain.Tests/Analyzer/Semantics/AstWhileStatementTest.cs
+++ Compiler/Domain.Tests/Analyzer/Semantics/AstWhileStatementTest.cs
@@ -2,16 +2,12 @@ using System;
 
 using KSPCompiler.Domain.Ast.Analyzers;
 using KSPCompiler.Domain.Ast.Analyzers.Evaluators.Conditionals;
-using KSPCompiler.Domain.Ast.Extensions;
 using KSPCompiler.Domain.Ast.Nodes;
 using KSPCompiler.Domain.Ast.Nodes.Blocks;
 using KSPCompiler.Domain.Ast.Nodes.Expressions;
-using KSPCompiler.Domain.Ast.Nodes.Extensions;
 using KSPCompiler.Domain.Ast.Nodes.Statements;
 using KSPCompiler.Domain.CompilerMessages;
 using KSPCompiler.Domain.Symbols.MetaData;
-using KSPCompiler.Domain.Symbols.MetaData.Extensions;
-using KSPCompiler.Resources;
 
 using NUnit.Framework;
 
@@ -93,76 +89,5 @@ public class AstWhileStatementTest
         };
 
         WhileStatementEvaluationTestBody( condition, 1 );
-    }
-
-}
-
-#region Work mock classes
-
-public class MockWhileStatementVisitor : DefaultAstVisitor
-{
-    private IWhileStatementEvaluator Evaluator { get; set; } = new MockWhileStatementEvaluator();
-
-    public void Inject( IWhileStatementEvaluator evaluator )
-    {
-        Evaluator = evaluator;
-    }
-
-    public override IAstNode Visit( AstWhileStatementNode node )
-        => Evaluator.Evaluate( this, node );
-}
-
-public class MockWhileStatementEvaluator : IWhileStatementEvaluator
-{
-    public IAstNode Evaluate( IAstVisitor visitor, AstWhileStatementNode statement )
-        => throw new NotImplementedException();
-}
-
-#endregion
-
-public interface IWhileStatementEvaluator : IConditionalStatementEvaluator<AstWhileStatementNode> {}
-
-public class WhileStatementEvaluator : IWhileStatementEvaluator
-{
-    private ICompilerMessageManger CompilerMessageManger { get; }
-
-    public WhileStatementEvaluator( ICompilerMessageManger compilerMessageManger )
-    {
-        CompilerMessageManger = compilerMessageManger;
-    }
-
-    public IAstNode Evaluate( IAstVisitor visitor, AstWhileStatementNode statement )
-    {
-        if( statement.Condition.IsNull() )
-        {
-            throw new AstAnalyzeException( statement, "Condition is Null instance" );
-        }
-
-        // 条件式の評価
-        if( statement.Condition.Accept( visitor ) is not AstExpressionNode evaluatedCondition )
-        {
-            throw new AstAnalyzeException( statement, "Failed to evaluate condition" );
-        }
-
-        // if条件式が条件式以外の場合
-        if( !evaluatedCondition.TypeFlag.IsBoolean() )
-        {
-            CompilerMessageManger.Error(
-                statement,
-                CompilerMessageResources.semantic_error_if_condition_incompatible
-            );
-
-            return statement.Clone<AstWhileStatementNode>();
-        }
-
-        // 真の場合のコードブロックの評価
-        statement.CodeBlock.AcceptChildren( visitor );
-
-        // Memo
-        // 畳み込みで条件式をリテラルでノードを置き換え可能だが
-        // 文法上は条件式リテラルが許されていないため、ここでは畳み込みは行わない
-        // 最適化やオブファスケーターの団で畳み込みを行う
-
-        return statement.Clone<AstWhileStatementNode>();
     }
 }
