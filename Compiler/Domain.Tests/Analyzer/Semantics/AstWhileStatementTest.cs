9556cdf5baf9c495b56848dd4f837d5684363028
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-11-02 11:54:48 +0900

    テスト、モック、本体の実装

--- Compiler/Domain.Tests/Analyzer/Semantics/AstWhileStatementTest.cs
+++ Compiler/Domain.Tests/Analyzer/Semantics/AstWhileStatementTest.cs
@@ -1,12 +1,17 @@
 using System;
 
+using KSPCompiler.Domain.Ast.Analyzers;
 using KSPCompiler.Domain.Ast.Analyzers.Evaluators.Conditionals;
+using KSPCompiler.Domain.Ast.Extensions;
 using KSPCompiler.Domain.Ast.Nodes;
 using KSPCompiler.Domain.Ast.Nodes.Blocks;
 using KSPCompiler.Domain.Ast.Nodes.Expressions;
+using KSPCompiler.Domain.Ast.Nodes.Extensions;
 using KSPCompiler.Domain.Ast.Nodes.Statements;
 using KSPCompiler.Domain.CompilerMessages;
 using KSPCompiler.Domain.Symbols.MetaData;
+using KSPCompiler.Domain.Symbols.MetaData.Extensions;
+using KSPCompiler.Resources;
 
 using NUnit.Framework;
 
@@ -69,11 +74,11 @@ public class AstWhileStatementTest
     [Test]
     public void CannotEvaluateWithoutConditionTest()
     {
-        // while() <-- no condition
+        // while() <-- FATAL: no condition (Syntax parser should not allow this)
         // end while
         var condition = NullAstExpressionNode.Instance;
 
-        WhileStatementEvaluationTestBody( condition, 1 );
+        Assert.Throws<AstAnalyzeException>( () => WhileStatementEvaluationTestBody( condition, 1 ) );
     }
 
     [Test]
@@ -128,6 +133,36 @@ public class WhileStatementEvaluator : IWhileStatementEvaluator
 
     public IAstNode Evaluate( IAstVisitor visitor, AstWhileStatementNode statement )
     {
-        throw new NotImplementedException();
+        if( statement.Condition.IsNull() )
+        {
+            throw new AstAnalyzeException( statement, "Condition is Null instance" );
+        }
+
+        // 条件式の評価
+        if( statement.Condition.Accept( visitor ) is not AstExpressionNode evaluatedCondition )
+        {
+            throw new AstAnalyzeException( statement, "Failed to evaluate condition" );
+        }
+
+        // if条件式が条件式以外の場合
+        if( !evaluatedCondition.TypeFlag.IsBoolean() )
+        {
+            CompilerMessageManger.Error(
+                statement,
+                CompilerMessageResources.semantic_error_if_condition_incompatible
+            );
+
+            return statement.Clone<AstWhileStatementNode>();
+        }
+
+        // 真の場合のコードブロックの評価
+        statement.CodeBlock.AcceptChildren( visitor );
+
+        // Memo
+        // 畳み込みで条件式をリテラルでノードを置き換え可能だが
+        // 文法上は条件式リテラルが許されていないため、ここでは畳み込みは行わない
+        // 最適化やオブファスケーターの団で畳み込みを行う
+
+        return statement.Clone<AstWhileStatementNode>();
     }
 }
