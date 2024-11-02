5d4e81c8ec7413ff9c0fc301d10f573885ed215f
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-11-02 22:28:48 +0900

    連結演算子の評価に絞ったユニットテストに修正

--- Compiler/Domain.Tests/Analyzer/Semantics/AstStringConcatenateOperatorTest.cs
+++ Compiler/Domain.Tests/Analyzer/Semantics/AstStringConcatenateOperatorTest.cs
@@ -1,12 +1,10 @@
 using System;
 
-using KSPCompiler.Domain.Ast.Analyzers.Evaluators.Convolutions.Strings;
 using KSPCompiler.Domain.Ast.Analyzers.Semantics;
 using KSPCompiler.Domain.Ast.Nodes;
 using KSPCompiler.Domain.Ast.Nodes.Expressions;
 using KSPCompiler.Domain.CompilerMessages;
-using KSPCompiler.Domain.Symbols;
-using KSPCompiler.Domain.Symbols.MetaData.Extensions;
+using KSPCompiler.Domain.Symbols.MetaData;
 
 using NUnit.Framework;
 
@@ -15,77 +13,79 @@ namespace KSPCompiler.Domain.Tests.Analyzer.Semantics;
 [TestFixture]
 public class AstStringConcatenateOperatorTest
 {
-    [Test]
-    public void StringConstantConcatenateOperator()
+    private static void ConcatenateOperatorTestBody( AstExpressionNode left, AstExpressionNode right, int expectedErrorCount )
     {
         var compilerMessageManger = ICompilerMessageManger.Default;
         var visitor = new MockAstStringConcatenateOperatorVisitor();
-        var variableTable = new VariableSymbolTable();
 
-        var integerConvolutionEvaluator = new StringConvolutionEvaluator(
-            visitor,
-            variableTable,
-            compilerMessageManger
-        );
-        var unaryOperatorEvaluator = new StringConcatenateOperatorEvaluator(
+        var evaluator = new StringConcatenateOperatorEvaluator(
             compilerMessageManger,
-            integerConvolutionEvaluator
+            new MockStringConvolutionEvaluator()
         );
 
-        visitor.Inject( unaryOperatorEvaluator );
+        visitor.Inject( evaluator );
 
         var operatorNode = new AstStringConcatenateExpressionNode
         {
-            Left  = new AstStringLiteralNode( "abc" ),
-            Right = new AstStringLiteralNode( "def" )
+            Left  = left,
+            Right = right
         };
 
-        var result = visitor.Visit( operatorNode );
+        visitor.Visit( operatorNode );
 
         compilerMessageManger.WriteTo( Console.Out );
 
-        var literal = result as AstStringLiteralNode;
-        Assert.IsTrue( literal is not null );
-        Assert.IsTrue( literal?.Value == "abcdef" );
-
-        Assert.IsFalse( compilerMessageManger.Count() > 0 );
+        Assert.AreEqual( expectedErrorCount, compilerMessageManger.Count() );
     }
 
     [Test]
-    public void StringConcatenateOperator()
+    public void StringConcatenateTest()
     {
-        var compilerMessageManger = ICompilerMessageManger.Default;
-        var visitor = new MockAstStringConcatenateOperatorVisitor();
-        var variableTable = new VariableSymbolTable();
-        var stringVariable = MockUtility.CreateStringVariable( "@str" );
-        variableTable.Add( stringVariable );
-
-        var integerConvolutionEvaluator = new StringConvolutionEvaluator(
-            visitor,
-            variableTable,
-            compilerMessageManger
+        // "abc" & "def"
+        ConcatenateOperatorTestBody(
+            new AstStringLiteralNode( "abc" ),
+            new AstStringLiteralNode( "def" ),
+            0
         );
-        var unaryOperatorEvaluator = new StringConcatenateOperatorEvaluator(
-            compilerMessageManger,
-            integerConvolutionEvaluator
-        );
-
-        visitor.Inject( unaryOperatorEvaluator );
-
-        var operatorNode = new AstStringConcatenateExpressionNode
-        {
-            Left  = new AstStringLiteralNode( "abc" ),
-            Right = MockUtility.CreateAstSymbolExpression( stringVariable )
-        };
-
-        var result = visitor.Visit( operatorNode ) as AstExpressionNode;
-
-        compilerMessageManger.WriteTo( Console.Out );
-
-        Assert.NotNull( result );
-        Assert.IsTrue( result?.TypeFlag.IsString() );
-        Assert.IsFalse( result?.Constant );
-        Assert.IsFalse( compilerMessageManger.Count() > 0 );
     }
 
+    [Test]
+    public void StringAndIntegerConcatenateTest()
+    {
+        // "abc" & 1
+        ConcatenateOperatorTestBody(
+            new AstStringLiteralNode( "abc" ),
+            new AstIntLiteralNode( 1 ),
+            0
+        );
+    }
+
+    [Test]
+    public void NonStringConcatenateTest()
+    {
+        // 1 & 1
+        ConcatenateOperatorTestBody(
+            new AstIntLiteralNode( 1 ),
+            new AstIntLiteralNode( 1 ),
+            0
+        );
+    }
+
+    [Test]
+    public void CannotConditionalAndStringConcatenateTest()
+    {
+        // ( 1 > 0 ) & "abc" <-- conditional (boolean) is not allowed
+        ConcatenateOperatorTestBody(
+            new AstGreaterThanExpressionNode
+            {
+                // make a evaluated node
+                // set an evaluated type value here because concatenating unit test only here
+                TypeFlag = DataTypeFlag.TypeBool,
+                Left     = new AstIntLiteralNode( 1 ),
+                Right    = new AstIntLiteralNode( 0 )
+            },
+            new AstStringLiteralNode( "abc" ),
+            1
+        );
+    }
 }
