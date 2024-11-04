88facae6743b26b9af03d47b8322c50f29b76a0a
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-11-04 22:37:02 +0900

    テストコード内で書き留めていた評価クラスを別ファイルに移動

--- Compiler/Domain.Tests/Analyzer/Preprocess/PreprocessTest.cs
+++ Compiler/Domain.Tests/Analyzer/Preprocess/PreprocessTest.cs
@@ -1,11 +1,8 @@
-using KSPCompiler.Domain.Ast.Analyzers;
-using KSPCompiler.Domain.Ast.Nodes;
+using KSPCompiler.Domain.Ast.Analyzers.Preprocessing;
 using KSPCompiler.Domain.Ast.Nodes.Blocks;
 using KSPCompiler.Domain.Ast.Nodes.Expressions;
 using KSPCompiler.Domain.Ast.Nodes.Statements;
-using KSPCompiler.Domain.Symbols;
 using KSPCompiler.Domain.Symbols.MetaData;
-using KSPCompiler.Domain.Symbols.MetaData.Extensions;
 
 using NUnit.Framework;
 
@@ -94,86 +91,5 @@ public class PreprocessTest
         analyzer.Traverse( ast );
 
         Assert.AreEqual( expectedIgnored, ifdef.Ignore );
-    }
-}
-
-#region Woprk mock classes
-//
-#endregion
-
-public class PreprocessAnalyzer : DefaultAstVisitor, IAstTraversal
-{
-    private IKspPreProcessorSymbolTable SymbolTable { get; }
-
-    public PreprocessAnalyzer( IKspPreProcessorSymbolTable symbolTable )
-    {
-        SymbolTable = symbolTable;
-    }
-
-    public void Traverse( AstCompilationUnitNode node )
-    {
-        node.AcceptChildren( this );
-    }
-
-    public override IAstNode Visit( AstKspPreprocessorDefineNode node )
-    {
-        var symbol = new KspPreProcessorSymbol
-        {
-            Name = node.Symbol.Name
-        };
-
-        SymbolTable.Add( symbol );
-
-        return node;
-    }
-
-    public override IAstNode Visit( AstKspPreprocessorUndefineNode node )
-    {
-        var symbol = new KspPreProcessorSymbol
-        {
-            Name = node.Symbol.Name
-        };
-
-        SymbolTable.Remove( symbol );
-
-        return node;
-    }
-
-    public override IAstNode Visit( AstKspPreprocessorIfdefineNode node )
-    {
-        if( node.Condition is not AstSymbolExpressionNode symbolNode )
-        {
-            throw new AstAnalyzeException( node, $"Currently only {nameof( AstSymbolExpressionNode )} is supported." );
-        }
-
-        if( !symbolNode.TypeFlag.IsPreprocessor() )
-        {
-            throw new AstAnalyzeException( node, $"Symbol is not a preprocessor symbol (={symbolNode.TypeFlag})" );
-        }
-
-        // シンボルが「見つからなければ」コードブロック自体を無視しても良いマークを行う
-        // この時点でコードブロックのノードは維持する
-        node.Ignore = !SymbolTable.TrySearchByName( symbolNode.Name, out _ );
-
-        return node;
-    }
-
-    public override IAstNode Visit( AstKspPreprocessorIfnotDefineNode node )
-    {
-        if( node.Condition is not AstSymbolExpressionNode symbolNode )
-        {
-            throw new AstAnalyzeException( node, $"Currently only {nameof( AstSymbolExpressionNode )} is supported." );
-        }
-
-        if( !symbolNode.TypeFlag.IsPreprocessor() )
-        {
-            throw new AstAnalyzeException( node, $"Symbol is not a preprocessor symbol (={symbolNode.TypeFlag})" );
-        }
-
-        // シンボルが「見つかれば」コードブロック自体を無視しても良いマークを行う
-        // この時点でコードブロックのノードは維持する
-        node.Ignore = SymbolTable.TrySearchByName( symbolNode.Name, out _ );
-
-        return node;
     }
 }
