fb7bcc99fe26a67f641629cf4b1084a4aefd1cba
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-11-10 23:22:23 +0900

    変数の状態評価をシンボル自体の評価から削除（参照先の式やステートメントで評価させる）

--- Compiler/Interactors/Analysis/Semantics/SymbolEvaluator.cs
+++ Compiler/Interactors/Analysis/Semantics/SymbolEvaluator.cs
@@ -81,21 +81,6 @@ public class SymbolEvaluator : ISymbolEvaluator
         // 変数への評価が確定するので参照済みフラグを立てる
         variable.Referenced = true;
 
-        // 変数は見つかったが、未初期化の場合はエラー
-        if( variable.State == SymbolState.UnInitialized )
-        {
-            CompilerMessageManger.Error(
-                expr,
-                CompilerMessageResources.semantic_error_variable_uninitialized,
-                variable.Name
-            );
-
-            // 変数は見つかったが、エラー扱いとして代替の評価結果を返す
-            result = CreateEvaluateNode( expr, variable, variable );
-            return true;
-        }
-
-        variable.State = SymbolState.Loaded;
         if( TryGetAstLiteralNode( variable, out result ) )
         {
             return true;
