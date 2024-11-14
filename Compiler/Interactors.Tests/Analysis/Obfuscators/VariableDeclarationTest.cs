d082598b17b5b6fcba0aee8f014e75fa1ea9d785
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-11-15 00:13:20 +0900

    UI配列変数宣言、初期値代入の難読化追加

--- Compiler/Interactors.Tests/Analysis/Obfuscators/VariableDeclarationTest.cs
+++ Compiler/Interactors.Tests/Analysis/Obfuscators/VariableDeclarationTest.cs
@@ -237,6 +237,60 @@ public class VariableDeclarationTest
         Assert.AreEqual( expected, output.ToString() );
     }
 
+    [Test]
+    public void ArrayUIInitializerTest()
+    {
+        // declare ui_table $v0[2] := (1, 2)
+
+        const string variableName = "%x";
+        const string obfuscatedName = "%v0";
+        const string expected = $"declare ui_table {obfuscatedName}[2] := (1, 2)";
+
+        var output = new StringBuilder();
+        var symbolTable = MockUtility.CreateAggregateSymbolTable();
+
+        var ui = MockUtility.CreateUITable();
+        symbolTable.UITypes.Add( ui );
+
+        var variable = MockUtility.CreateVariable( variableName, DataTypeFlag.TypeIntArray );
+        variable.Modifier = ModifierFlag.UI;
+        variable.UIType   = ui;
+
+        symbolTable.Variables.Add( variable );
+
+        var obfuscatedTable = new ObfuscatedVariableTable( symbolTable.Variables, "v" );
+
+        var uiInitializer = new AstExpressionListNode();
+        uiInitializer.Expressions.Add( new AstIntLiteralNode( 1 ) );
+        uiInitializer.Expressions.Add( new AstIntLiteralNode( 2 ) );
+
+        var arraySize = new AstArrayInitializerNode
+        {
+            Size = new AstIntLiteralNode( 2 )
+        };
+
+        var node = new AstVariableDeclarationNode
+        {
+            Name = variableName,
+            Initializer = new AstVariableInitializerNode
+            {
+                ArrayInitializer = arraySize,
+                PrimitiveInitializer = new AstPrimitiveInitializerNode(
+                    NullAstNode.Instance,
+                    NullAstExpressionNode.Instance,
+                    uiInitializer
+                )
+            }
+        };
+
+        var evaluator = new VariableDeclarationEvaluator( output, symbolTable.Variables, symbolTable.UITypes, obfuscatedTable );
+        var visitor = new MockVariableDeclarationVisitor( output );
+
+        visitor.Inject( evaluator );
+        visitor.Visit( node );
+
+        Assert.AreEqual( expected, output.ToString() );
+    }
 }
 
 public class MockVariableDeclarationVisitor : DefaultAstVisitor
@@ -392,7 +446,7 @@ public class VariableDeclarationEvaluator : IVariableDeclarationEvaluator
     private void OutputArrayBasedUIInitializer( IAstVisitor visitor, AstVariableDeclarationNode node, AstArrayInitializerNode initializer, VariableSymbol variable )
     {
         OutputArraySize( visitor, node, initializer, variable );
-        OutputUIArguments( visitor, node, initializer.Initializer, variable.UIType );
+        OutputUIArguments( visitor, node, node.Initializer.PrimitiveInitializer.UIInitializer, variable.UIType );
     }
 
     private void OutputPrimitiveBasedUIInitializer( IAstVisitor visitor, AstVariableDeclarationNode node, AstExpressionListNode initializer, VariableSymbol variable )
