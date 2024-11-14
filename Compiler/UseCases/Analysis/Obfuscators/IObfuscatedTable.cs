e242e316df810738e3f76cd697e05cf2c2884b98
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-11-15 00:30:07 +0900

    ファイル分割

--- Compiler/UseCases/Analysis/Obfuscators/IObfuscatedTable.cs
+++ Compiler/UseCases/Analysis/Obfuscators/IObfuscatedTable.cs
@@ -7,6 +7,3 @@ public interface IObfuscatedTable<in TSymbol> where TSymbol : SymbolBase
     bool TryGetObfuscatedByName( string original, out string result );
     string GetObfuscatedByName( string original );
 }
-
-public interface IObfuscatedVariableTable : IObfuscatedTable<VariableSymbol> {}
-public interface IObfuscatedUserFunctionTable : IObfuscatedTable<UserFunctionSymbol> {}
