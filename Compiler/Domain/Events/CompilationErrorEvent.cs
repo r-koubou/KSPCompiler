9de07d0c69c40da2576f9e7844b73fadec2b149c
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-12-01 22:36:17 +0900

    Positionをメンバとして持つ構成に習性

--- Compiler/Domain/Events/CompilationErrorEvent.cs
+++ Compiler/Domain/Events/CompilationErrorEvent.cs
@@ -1,8 +1,28 @@
+using KSPCompiler.Commons.Text;
+
 namespace KSPCompiler.Domain.Events;
 
-public readonly struct CompilationErrorEvent( string message, int line = 0, int column = 0 ) : IEvent
+public readonly struct CompilationErrorEvent : IEvent
 {
-    public string Message { get; } = message;
-    public int Line { get; } = line;
-    public int Column { get; } = column;
+    public string Message { get; }
+
+    public Position Position { get; }
+
+    public CompilationErrorEvent( string message, int line = 0, int column = 0 )
+    {
+        Message = message;
+        Position = new Position
+        {
+            BeginLine   = line,
+            EndLine     = line,
+            BeginColumn = column,
+            EndColumn   = column
+        };
+    }
+
+    public CompilationErrorEvent( string message, Position position )
+    {
+        Message  = message;
+        Position = position;
+    }
 }
