11756b5cfd96f0bf376a4e776b19c42e07ed39f1
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2025-01-02 21:08:46 +0900

    == != 演算子オーバーロード実装

--- Compiler/Commons/Text/Position.cs
+++ Compiler/Commons/Text/Position.cs
@@ -1,7 +1,9 @@
+using System;
+
 namespace KSPCompiler.Commons.Text
 {
     // ReSharper disable InconsistentNaming
-    public struct Position
+    public struct Position : IEquatable<Position>
     {
         public static Position Zero { get; } = new()
         {
@@ -30,6 +32,43 @@ namespace KSPCompiler.Commons.Text
         /// End Column Number. It's -1 if it's unknown.
         /// </summary>
         public Column EndColumn;
+
+        public bool Equals( Position other )
+            => this == other;
+
+        public override bool Equals( object? obj )
+        {
+            if( obj is null )
+            {
+                return false;
+            }
+
+            return obj is Position position && Equals( position );
+        }
+
+        public override int GetHashCode()
+            => HashCode.Combine(
+                BeginLine.Value,
+                EndLine.Value,
+                BeginColumn.Value,
+                EndColumn.Value
+            );
+
+        #region Operators
+        public static bool operator ==(Position a, Position b)
+        {
+            return a.BeginLine.Value == b.BeginLine.Value
+                     && a.EndLine.Value == b.EndLine.Value
+                     && a.BeginColumn.Value == b.BeginColumn.Value
+                     && a.EndColumn.Value == b.EndColumn.Value;
+        }
+
+        public static bool operator !=(Position a, Position b)
+        {
+            return !(a == b);
+        }
+        #endregion
+
     }
     // ReSharper restore InconsistentNaming
 }
