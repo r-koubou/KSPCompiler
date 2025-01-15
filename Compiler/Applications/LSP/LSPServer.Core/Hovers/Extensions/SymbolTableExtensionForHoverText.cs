27571baa57117e4fd8c7929baf5e37e812e8f473
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2025-01-16 01:18:23 +0900

    Signaturehelpのホバー表示でも使用するため、Hoverのテキスト生成処理のロジックを疎結合にした

---
+++ Compiler/Applications/LSP/LSPServer.Core/Hovers/Extensions/SymbolTableExtensionForHoverText.cs
@@ -0,0 +1,28 @@
+using KSPCompiler.Domain.Symbols;
+
+namespace KSPCompiler.LSPServer.Core.Hovers.Extensions;
+
+public static class SymbolTableExtensionForHoverText
+{
+    public static bool TryBuildHoverText<TSymbol>( this ISymbolTable<TSymbol> self, string symbolName, out string result, IHoverTextBuilder<TSymbol>? builder = null )
+        where TSymbol : SymbolBase
+    {
+        result = string.Empty;
+
+        if( !self.TrySearchByName( symbolName, out var symbol ) )
+        {
+            return false;
+        }
+
+        if( builder == null )
+        {
+            result = symbol.Description.Value;
+
+            return true;
+        }
+
+        result = builder.BuildHoverText( symbol );
+
+        return !string.IsNullOrEmpty( result );
+    }
+}
--- Compiler/Applications/LSP/LSPServer.Core/Hovers/HoverService.cs
+++ Compiler/Applications/LSP/LSPServer.Core/Hovers/HoverService.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 using KSPCompiler.Domain.Symbols;
-using KSPCompiler.Domain.Symbols.MetaData.Extensions;
 using KSPCompiler.LSPServer.Core.Compilations;
+using KSPCompiler.LSPServer.Core.Hovers.Extensions;
 
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 
@@ -20,52 +16,47 @@ public sealed class HoverService( CompilerCacheService compilerCacheService )
     public async Task<Hover?> HandleAsync( HoverParams request, CancellationToken cancellationToken )
     {
         var cache = CompilerCacheService.GetCache( request.TextDocument.Uri );
+        var symbols = cache.SymbolTable;
         var word = DocumentUtility.ExtractWord( cache.AllLinesText, request.Position );
 
         #region User deffinitions
         // ユーザー定義変数(コメントがある場合)
-        if( TryBuildHoverForUserDefinition( cache.SymbolTable.UserVariables, word, out var hover ) )
+        if( symbols.UserVariables.TryBuildHoverText(word, out var hoverText, new UserDefinedSymbolHoverTextBuilder<VariableSymbol>() ) )
         {
-            return hover;
+            return hoverText.AsHover();
         }
 
         // ユーザー定義関数(コメントがある場合)
-        if( TryBuildHoverForUserDefinition( cache.SymbolTable.UserFunctions, word, out hover ) )
+        if( symbols.UserFunctions.TryBuildHoverText(word, out hoverText, new UserDefinedSymbolHoverTextBuilder<UserFunctionSymbol>() ) )
         {
-            return hover;
+            return hoverText.AsHover();
         }
 
         // ユーザー定義コールバック(コメントがある場合)
-        if( TryBuildHoverForUserDefinition( cache.SymbolTable.UserCallbacks, word, out hover ) )
+        if( symbols.UserCallbacks.TryBuildHoverText(word, out hoverText, new UserDefinedSymbolHoverTextBuilder<CallbackSymbol>() ) )
         {
-            return hover;
+            return hoverText.AsHover();
         }
 
         #endregion ~User deffinitions
 
         #region BuiltIn
         // ビルトイン変数
-        if( TryBuildHover( cache.SymbolTable.BuiltInVariables, word, out hover ) )
+        if( symbols.BuiltInVariables.TryBuildHoverText( word, out hoverText, new VariableHoverTextBuilder() ) )
         {
-            return hover;
+            return hoverText.AsHover();
         }
 
         // UI型
-        if( TryBuildHover( cache.SymbolTable.UITypes, word, out hover, GetUIypeHoverText ) )
+        if( symbols.UITypes.TryBuildHoverText( word, out hoverText, new UITypeHoverTextBuilder() ) )
         {
-            return hover;
+            return hoverText.AsHover();
         }
 
         // コマンド
-        if( TryBuildHover( cache.SymbolTable.Commands, word, out hover, GetCommandHoverText ) )
+        if( symbols.Commands.TryBuildHoverText( word, out hoverText, new CommandHoverTextBuilder() ) )
         {
-            return hover;
-        }
-
-        // ユーザー定義コールバック(コメントが無い場合はビルトインのコールバックのDescriptionを表示)
-        if( TryBuildHover( cache.SymbolTable.BuiltInCallbacks, word, out hover ) )
-        {
-            return hover;
+            return hoverText.AsHover();
         }
 
         #endregion ~BuiltIn
@@ -73,166 +64,5 @@ public sealed class HoverService( CompilerCacheService compilerCacheService )
         await Task.CompletedTask;
 
         return null;
-    }
-
-    private static bool TryBuildHover<TSymbol>(
-        ISymbolTable<TSymbol> symbols,
-        string word,
-        out Hover? result,
-        Func<TSymbol, string>? hoverTextBuilder = null )
-        where TSymbol : SymbolBase
-    {
-        result = null;
-
-        if( !symbols.TrySearchByName( word, out var symbol ) )
-        {
-            return false;
-        }
-
-        var hoverText = symbol.Description.Value;
-
-        if( hoverTextBuilder!= null )
-        {
-            hoverText = hoverTextBuilder( symbol );
-        }
-
-        if( string.IsNullOrWhiteSpace( hoverText ) )
-        {
-            return false;
-        }
-
-        result = new Hover
-        {
-            Contents = new MarkedStringsOrMarkupContent(
-                new MarkedString( hoverText )
-            )
-        };
-
-        return true;
-    }
-
-    private static bool TryBuildHoverForUserDefinition<TSymbol>(
-        ISymbolTable<TSymbol> symbols,
-        string word,
-        out Hover? result )
-        where TSymbol : SymbolBase
-    {
-        return TryBuildHover(
-            symbols,
-            word,
-            out result,
-            symbol => BuildHoverTextFromUserComment( symbol.CommentLines )
-        );
-    }
-
-    private static string GetUIypeHoverText( UITypeSymbol uiTypeSymbol )
-    {
-        var builder = new StringBuilder( 64 );
-
-        builder.AppendLine( $"**{uiTypeSymbol.Name}**" );
-        builder.AppendLine();
-        builder.AppendLine( uiTypeSymbol.Description.Value );
-
-        return builder.ToString();
-    }
-
-    private static string GetCommandHoverText( CommandSymbol commandSymbol )
-    {
-        var builder = new StringBuilder( 64 );
-
-        /*
-         * command_name(arg1, arg2, ...)
-         * Description
-         */
-
-        builder.Append( $"**{commandSymbol.Name}" );
-
-        if( commandSymbol.ArgumentCount > 0 )
-        {
-            var index = 0;
-            var argCount = commandSymbol.Arguments.Count;
-
-            builder.Append( "(" );
-
-            foreach( var arg in commandSymbol.Arguments )
-            {
-                builder.Append( $"{arg.Name}" );
-
-                if( index < argCount - 1 )
-                {
-                    builder.Append( ", " );
-                }
-
-                index++;
-            }
-
-            builder.Append( ")" );
-            builder.AppendLine( "**" );
-        }
-        else
-        {
-            builder.AppendLine( "**" );
-            builder.AppendLine();
-        }
-
-        builder.AppendLine();
-        builder.AppendLine( commandSymbol.Description.Value );
-
-        if( commandSymbol.ArgumentCount == 0 )
-        {
-            return builder.ToString();
-        }
-
-        builder.AppendLine();
-
-        /*
-         * Arguments:
-         *   - arg1
-         *     - type1
-         *  - arg2
-         *    - type2
-         * :
-         * :
-         */
-        builder.AppendLine( "**Arguments:**" );
-
-        foreach( var arg in commandSymbol.Arguments )
-        {
-            var argType = arg.DataType.ToMessageString();
-
-            builder.AppendLine( $"- {arg.Name}" );
-
-            if( arg.UITypeNames.Count > 0 )
-            {
-                builder.AppendLine( $"  - {string.Join( ", ", arg.UITypeNames )}" );
-            }
-            else
-            {
-                builder.AppendLine( $"  - {argType}" );
-            }
-        }
-
-        return builder.ToString();
-    }
-
-    private static string BuildHoverTextFromUserComment( IReadOnlyCollection<string> commentLines )
-    {
-        var builder = new StringBuilder( 64 );
-
-        var  minIndent = commentLines
-                        .Where( line => line.Trim().Length > 0 )
-                        .Select( line => line.Length - line.TrimStart().Length )
-                        .DefaultIfEmpty( 0 )
-                        .Min();
-
-        var normalizedLines = commentLines
-           .Select( line => line.Length >= minIndent ? line.Substring( minIndent ) : line );
-
-        foreach( var line in normalizedLines )
-        {
-            builder.AppendLine( line );
-        }
-
-        return builder.ToString();
     }
 }
