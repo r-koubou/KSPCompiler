10c0b6f342dba863e182997fbdcc46ea8eab2a6f
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2025-01-19 15:36:29 +0900

    developのコードに戻した

--- Compiler/Commons/ValueObjects/IValueObject.cs
+++ Compiler/Commons/ValueObjects/IValueObject.cs
@@ -1,8 +1,6 @@
-using System;
-
 namespace KSPCompiler.Commons.ValueObjects;
 
-public interface IValueObject<TValue> : IEquatable<TValue> where TValue : notnull
+public interface IValueObject<TValue> where TValue : notnull
 {
     TValue Value { get; init; }
 }
