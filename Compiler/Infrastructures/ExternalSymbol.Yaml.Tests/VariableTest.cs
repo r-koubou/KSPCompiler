4de79b709f018fcf63af5a99856ab8d902f95cb1
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2025-01-27 23:52:50 +0900

    テスト作成

---
+++ Compiler/Infrastructures/ExternalSymbol.Yaml.Tests/VariableTest.cs
@@ -0,0 +1,75 @@
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using KSPCompiler.Commons.Path;
+using KSPCompiler.Domain.Symbols;
+using KSPCompiler.ExternalSymbol.Commons;
+using KSPCompiler.ExternalSymbol.Yaml.Variables;
+using KSPCompiler.Gateways.Symbols;
+using KSPCompiler.Infrastructures.Commons.LocalStorages;
+
+using NUnit.Framework;
+
+namespace KSPCompiler.ExternalSymbol.Yaml.Tests;
+
+[TestFixture]
+public class VariableTest
+{
+    private static readonly string TestDataDirectory = Path.Combine( "TestData", "VariableTest" );
+
+    private static ISymbolImporter<VariableSymbol> CreateLocalImporter( string path )
+    {
+        var reader = new LocalTextContentReader( new FilePath( path ) );
+        return new VariableSymbolImporter( reader );
+    }
+
+    private static ISymbolExporter<VariableSymbol> CreateLocalExporter( string path )
+    {
+        var writer = new LocalTextContentWriter( new FilePath( path ) );
+        return new VariableSymbolExporter( writer );
+    }
+
+    [Test]
+    public void TranslateSymbolTable()
+    {
+        var path = Path.Combine( TestDataDirectory, "variable.yaml" );
+        var importer = CreateLocalImporter( path );
+        var symbols = importer.Import();
+
+        Assert.That( symbols.Count, Is.EqualTo( 2 ) );
+        Assert.That( symbols.First().BuiltIntoVersion, Is.EqualTo( SymbolBuiltIntoVersion.NotAvailable ) );
+    }
+
+    [Test]
+    public async Task TranslateSymbolTableAsync()
+    {
+        var path = Path.Combine( TestDataDirectory, "variable.yaml" );
+        var importer = CreateLocalImporter( path );
+
+        await Task.Run( async () => {
+            var symbols = await importer.ImportAsync();
+            Assert.That( symbols.Count, Is.EqualTo( 2 ) );
+        });
+    }
+
+    [Test]
+    public void StoreTest()
+    {
+        var path = Path.Combine( TestDataDirectory, "variable_out.yaml" );
+        var exporter = CreateLocalExporter( path );
+        var symbolTable = MockSymbolTableUtility.CreateDummyVariableSymbols();
+
+        exporter.ExportAsync( symbolTable );
+    }
+
+    [Test]
+    public async Task StoreAsyncTest()
+    {
+        var path = Path.Combine( TestDataDirectory, "variable_out.yaml" );
+        var exporter = CreateLocalExporter( path );
+        var symbolTable = MockSymbolTableUtility.CreateDummyVariableSymbols();
+
+        await exporter.ExportAsync( symbolTable );
+    }
+}
