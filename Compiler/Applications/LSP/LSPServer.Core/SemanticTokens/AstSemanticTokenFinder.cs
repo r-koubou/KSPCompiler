09f6a06117053b77b0f2d26c8247b139520c3b10
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2025-02-02 18:41:42 +0900

    トークン位置情報の計算を実装

--- Compiler/Applications/LSP/LSPServer.Core/SemanticTokens/AstSemanticTokenFinder.cs
+++ Compiler/Applications/LSP/LSPServer.Core/SemanticTokens/AstSemanticTokenFinder.cs
@@ -23,9 +23,15 @@ public class AstSemanticTokenFinder(
     private ImmutableArray<SemanticTokenModifier> LegendTokenModifiers { get; } = legendTokenModifiers;
     private List<int> Result { get; } = [];
 
+    private int lastLine = 0;
+    private int lastColumn = 0;
+
     public ImmutableArray<int> Find( AstCompilationUnitNode ast )
     {
         Result.Clear();
+        lastLine = 0;
+        lastColumn = 0;
+
         ast.AcceptChildren( this );
 
         return ImmutableArray.Create( Result.ToArray() );
@@ -33,73 +39,65 @@ public class AstSemanticTokenFinder(
 
     private void Add( KSPCompilerPosition position, SemanticTokenType tokenType )
     {
-        Result.Add( position.BeginLine.Value - 1 );
-        Result.Add( position.BeginColumn.Value );
-        Result.Add( position.EndColumn.Value - position.BeginColumn.Value );
+        var deltaLine = 0;
+        var deltaColumn = 0;
+
+        if( lastLine != position.BeginLine.Value - 1 )
+        {
+            deltaLine = position.BeginLine.Value - lastLine - 1;
+            deltaColumn = position.BeginColumn.Value;
+        }
+        else
+        {
+            deltaColumn = position.BeginColumn.Value - lastColumn;
+        }
+
+        var length = position.EndColumn.Value - position.BeginColumn.Value;
+
+        Result.Add( deltaLine );
+        Result.Add( deltaColumn );
+        Result.Add( length );
         Result.Add( LegendTokenTypes.IndexOf( tokenType ) );
         Result.Add( 0 );
+
+        lastLine   = position.BeginLine.Value - 1;
+        lastColumn = position.BeginColumn.Value;
     }
 
-    private void Add( KSPCompilerPosition begin, KSPCompilerPosition end, SemanticTokenType tokenType )
-    {
-        Result.Add( begin.BeginLine.Value - 1 );
-        Result.Add( begin.BeginColumn.Value );
-        Result.Add( end.EndColumn.Value - begin.BeginColumn.Value );
-        Result.Add( LegendTokenTypes.IndexOf( tokenType ) );
-        Result.Add( 0 );
-    }
 
-    public override IAstNode Visit( AstSymbolExpressionNode node )
-    {
-        if( SymbolTable.BuiltInVariables.TrySearchByName( node.Name, out var builtInVariable ) )
-        {
-            Add( node.Position, SemanticTokenType.Variable );
-        }
+    // public override IAstNode Visit( AstCallbackDeclarationNode node )
+    // {
+    //     if( SymbolTable.UserCallbacks.TrySearchByName( node.Name, out _ ) )
+    //     {
+    //         // on
+    //         Add( node.BeginOnKeywordPosition, SemanticTokenType.Keyword );
+    //
+    //         // callback name
+    //         Add( node.NamePosition, SemanticTokenType.Keyword );
+    //
+    //         // end on
+    //         Add( node.EndKeywordPosition, SemanticTokenType.Keyword );
+    //         Add( node.EndOnKeywordPosition, SemanticTokenType.Keyword );
+    //     }
+    //
+    //     return base.Visit( node );
+    // }
 
-        return base.Visit( node );
-    }
-
-    public override IAstNode Visit( AstCallCommandExpressionNode node )
-    {
-        if( SymbolTable.Commands.TrySearchByName( node.Left.Name, out var commandSymbol ) )
-        {
-            Add( node.Position, SemanticTokenType.Method );
-        }
-
-        return base.Visit( node );
-    }
-
-    public override IAstNode Visit( AstCallbackDeclarationNode node )
-    {
-        if( SymbolTable.UserCallbacks.TrySearchByName( node.Name, out _ ) )
-        {
-            // on
-            Add( node.BeginOnKeywordPosition, SemanticTokenType.Keyword );
-
-            // callback name
-            Add( node.NamePosition, SemanticTokenType.Method );
-
-            // end on
-            Add( node.EndKeywordPosition, node.EndOnKeywordPosition, SemanticTokenType.Keyword );
-        }
-
-        return base.Visit( node );
-    }
-
-    public override IAstNode Visit( AstUserFunctionDeclarationNode node )
-    {
-        if( SymbolTable.UserFunctions.TrySearchByName( node.Name, out _ ) )
-        {
-            // function
-            Add( node.BeginOnKeywordPosition, SemanticTokenType.Keyword );
-
-            // function name
-            Add( node.NamePosition, SemanticTokenType.Keyword );
-
-            // end function
-            Add( node.EndKeywordPosition, node.EndOnKeywordPosition, SemanticTokenType.Keyword );
-        }
-
-        return base.Visit( node );
-    }
+    // public override IAstNode Visit( AstUserFunctionDeclarationNode node )
+    // {
+    //     if( SymbolTable.UserFunctions.TrySearchByName( node.Name, out _ ) )
+    //     {
+    //         // function
+    //         Add( node.BeginOnKeywordPosition, SemanticTokenType.Keyword );
+    //
+    //         // function name
+    //         Add( node.NamePosition, SemanticTokenType.Keyword );
+    //
+    //         // end function
+    //         Add( node.EndKeywordPosition, SemanticTokenType.Keyword );
+    //         Add( node.EndOnKeywordPosition, SemanticTokenType.Keyword );
+    //     }
+    //
+    //     return base.Visit( node );
+    // }
 }
