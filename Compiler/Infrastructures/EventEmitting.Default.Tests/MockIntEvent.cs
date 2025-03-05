6f3a3f7f3225369bf143a716e0327a2d936f7755
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2025-03-06 00:10:45 +0900

    不要ファイルの削除

--- Compiler/Infrastructures/EventEmitting.Default.Tests/MockIntEvent.cs
+++
@@ -1,8 +0,0 @@
-using KSPCompiler.Gateways.EventEmitting;
-
-namespace KSPCompiler.Infrastructures.EventEmitting.Default.Tests;
-
-public class MockIntEvent( int value ) : IEvent
-{
-    public int Value { get; } = value;
-}
