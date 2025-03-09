ea6faf868c02cf7bcaee7e6739161d82e8da99ff
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2025-03-10 00:03:37 +0900

    不要ファイル削除

--- Compiler/Features/Compilation/Infrastructures/SymbolRepository.Yaml/Variables/Models/VariableSymbolRootModel.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace KSPCompiler.Features.Compilation.Infrastructures.SymbolRepository.Yaml.Variables.Models;
-
-public sealed class VariableSymbolRootModel : ISymbolRootModel<VariableSymbolModel>
-{
-    public string FormatVersion { get; set; } = "1.0.0";
-    public List<VariableSymbolModel> Data { get; set; } = [];
-}
