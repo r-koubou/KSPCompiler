d46993fec1b2e54bc85ef1b894427c1af87b4dcd
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2025-03-13 23:22:35 +0900

    不必要に型の接頭文字を除外していたため削除

--- Compiler/Features/LanguageServer/UseCase/Completion/CompletionInteractor.cs
+++ Compiler/Features/LanguageServer/UseCase/Completion/CompletionInteractor.cs
@@ -8,7 +8,6 @@ using System.Threading.Tasks;
 using KSPCompiler.Features.LanguageServer.UseCase.Abstractions;
 using KSPCompiler.Features.LanguageServer.UseCase.Abstractions.Completion;
 using KSPCompiler.Shared.Domain.Compilation.Symbols;
-using KSPCompiler.Shared.Domain.Compilation.Symbols.MetaData;
 using KSPCompiler.Shared.Domain.Compilation.Symbols.MetaData.Extensions;
 
 namespace KSPCompiler.Features.LanguageServer.UseCase.Completion;
@@ -244,11 +243,6 @@ public sealed class CompletionInteractor : ICompletionUseCase
         if( !variableSymbol.BuiltIn && variableSymbol.Modifier.IsConstant() )
         {
             kind = CompletionItemKind.Constant;
-        }
-
-        if( DataTypeUtility.StartsWithDataTypeCharacter( partialName ) )
-        {
-            insertText = variableSymbol.Name.Value[ 1.. ];
         }
 
         return new CompletionItem(
