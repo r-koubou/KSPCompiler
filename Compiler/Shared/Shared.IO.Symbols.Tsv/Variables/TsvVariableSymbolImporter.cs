f698d9388c1d4e398b81a640899efc265a999fa8
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2025-03-15 22:43:56 +0900

    Variableのパラメトリックポリモーフィズムへ対応

--- Compiler/Shared/Shared.IO.Symbols.Tsv/Variables/TsvVariableSymbolImporter.cs
+++ Compiler/Shared/Shared.IO.Symbols.Tsv/Variables/TsvVariableSymbolImporter.cs
@@ -1,27 +1,8 @@
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-
 using KSPCompiler.Shared.Domain.Compilation.Symbols;
 using KSPCompiler.Shared.IO.Abstractions.Contents;
-using KSPCompiler.Shared.IO.Abstractions.Symbol;
 using KSPCompiler.Shared.IO.Symbols.Tsv.Variables.Translators;
 
 namespace KSPCompiler.Shared.IO.Symbols.Tsv.Variables;
 
-public class TsvVariableSymbolImporter : ISymbolImporter<VariableSymbol>
-{
-    private readonly ITextContentReader contentReader;
-
-    public TsvVariableSymbolImporter( ITextContentReader reader )
-    {
-        contentReader = reader;
-    }
-
-    public async Task<IReadOnlyCollection<VariableSymbol>> ImportAsync( CancellationToken cancellationToken = default )
-    {
-        var tsv = await contentReader.ReadContentAsync( cancellationToken );
-
-        return new TsvToSymbolTranslator().Translate( tsv );
-    }
-}
+public class TsvVariableSymbolImporter( ITextContentReader reader )
+    : TsvSymbolImporter<VariableSymbol, TsvToVariableSymbolTranslator>( reader );
