e362866666bd1838a85e6891c64e7bbfb7776ad4
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2025-03-15 22:33:26 +0900

    Command のパラメトリックポリモーフィズムへ対応

--- Compiler/Shared/Shared.IO.Symbols.Tsv/Commands/TsvCommandSymbolImporter.cs
+++ Compiler/Shared/Shared.IO.Symbols.Tsv/Commands/TsvCommandSymbolImporter.cs
@@ -1,27 +1,8 @@
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-
 using KSPCompiler.Shared.Domain.Compilation.Symbols;
 using KSPCompiler.Shared.IO.Abstractions.Contents;
-using KSPCompiler.Shared.IO.Abstractions.Symbol;
 using KSPCompiler.Shared.IO.Symbols.Tsv.Commands.Translators;
 
 namespace KSPCompiler.Shared.IO.Symbols.Tsv.Commands;
 
-public class TsvCommandSymbolImporter : ISymbolImporter<CommandSymbol>
-{
-    private readonly ITextContentReader contentReader;
-
-    public TsvCommandSymbolImporter( ITextContentReader reader )
-    {
-        contentReader = reader;
-    }
-
-    public async Task<IReadOnlyCollection<CommandSymbol>> ImportAsync( CancellationToken cancellationToken = default )
-    {
-        var tsv = await contentReader.ReadContentAsync( cancellationToken );
-
-        return new TsvToSymbolTranslator().Translate( tsv );
-    }
-}
+public class TsvCommandSymbolImporter( ITextContentReader reader )
+    : TsvSymbolImporter<CommandSymbol, TsvToCommandSymbolTranslator>( reader );
