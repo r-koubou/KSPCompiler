d9c338f5268b25635e3be75e47ce335d411667c3
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2025-04-01 00:00:26 +0900

    ユーザー宣言直前にコメントテキストを残している場合はそれを、そうでない場合はビルトイン（Description）をホバーや補完候補のテキストに使用できる関数を共通化

--- Compiler/Features/LanguageServer/UseCase/Hover/UserDefinedSymbolHoverTextBuilder.cs
+++ Compiler/Features/LanguageServer/UseCase/Hover/UserDefinedSymbolHoverTextBuilder.cs
@@ -1,6 +1,3 @@
-using System.Linq;
-using System.Text;
-
 using KSPCompiler.Shared.Domain.Compilation.Symbols;
 
 namespace KSPCompiler.Features.LanguageServer.UseCase.Hover;
@@ -9,23 +6,6 @@ public class UserDefinedSymbolHoverTextBuilder<TSymbol> : IHoverTextBuilder<TSym
 {
     public string BuildHoverText( TSymbol symbol )
     {
-        var builder = new StringBuilder( 64 );
-        var commentLines = symbol.CommentLines;
-
-        var minIndent = commentLines
-                       .Where( line => line.Trim().Length > 0 )
-                       .Select( line => line.Length - line.TrimStart().Length )
-                       .DefaultIfEmpty( 0 )
-                       .Min();
-
-        var normalizedLines = commentLines
-           .Select( line => line.Length >= minIndent ? line.Substring( minIndent ) : line );
-
-        foreach( var line in normalizedLines )
-        {
-            builder.AppendLine( line );
-        }
-
-        return builder.ToString();
+        return DocumentUtility.GetCommentOrDescriptionText( symbol );
     }
 }
