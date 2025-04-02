130bb4e0fc955a7ea4f9c645e2732f625b451c75
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2025-04-03 00:44:31 +0900

    辞書のキーをインスタンスの参照ではなくUri文字列で判定するように修正

--- Compiler/Features/LanguageServer/UseCase/Compilation/CompilationCacheManager.cs
+++ Compiler/Features/LanguageServer/UseCase/Compilation/CompilationCacheManager.cs
@@ -7,24 +7,27 @@ namespace KSPCompiler.Features.LanguageServer.UseCase.Compilation;
 
 public sealed class CompilationCacheManager : ICompilationCacheManager
 {
-    private readonly ConcurrentDictionary<ScriptLocation, CompilationCacheItem> symbolTableCache = new();
+    private readonly ConcurrentDictionary<string, CompilationCacheItem> symbolTableCache = new();
+
+    private static string GetCacheKey( ScriptLocation scriptLocation )
+        => scriptLocation.ScriptUri.ToString();
 
     public bool ContainsCache( ScriptLocation scriptLocation )
-        => symbolTableCache.ContainsKey( scriptLocation );
+        => symbolTableCache.ContainsKey( GetCacheKey( scriptLocation ) );
 
     public CompilationCacheItem GetCache( ScriptLocation scriptLocation )
     {
         return symbolTableCache.GetOrAdd(
-            scriptLocation,
+            GetCacheKey( scriptLocation ),
             new CompilationCacheItem( scriptLocation )
         );
     }
 
     public void UpdateCache( ScriptLocation scriptLocation, CompilationCacheItem newCacheItem )
     {
-        symbolTableCache[ scriptLocation ] = newCacheItem;
+        symbolTableCache[ GetCacheKey( scriptLocation ) ] = newCacheItem;
     }
 
-    public bool RemoveCache( ScriptLocation uri )
-        => symbolTableCache.TryRemove( uri, out _ );
+    public bool RemoveCache( ScriptLocation scriptLocation )
+        => symbolTableCache.TryRemove( GetCacheKey( scriptLocation ), out _ );
 }
