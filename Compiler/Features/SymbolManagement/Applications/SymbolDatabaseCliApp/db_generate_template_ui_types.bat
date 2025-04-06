04afcd881ad00d8605adaa65960e7bf4387ce039
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2025-04-06 23:45:05 +0900

    シンボルデータ管理起動用シェルスクリプト作成

---
+++ Compiler/Features/SymbolManagement/Applications/SymbolDatabaseCliApp/db_generate_template_ui_types.bat
@@ -0,0 +1,9 @@
+@echo off
+
+setlocal
+
+set THIS_KSPC_DIR=%~dp0
+
+dotnet "%THIS_KSPC_DIR%\KSPCompiler.Features.SymbolManagement.Applications.SymbolDbManager.dll" generate-template-ui-types %*
+
+endlocal
