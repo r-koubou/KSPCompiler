37c7181259a9ce8c86d987b975461de7675449f0
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2025-04-06 22:17:10 +0900

    名前空間のtypo修正

--- Compiler/Features/Compilation/Applications/Compilation.Cli/Main.cs
+++ Compiler/Features/Compilation/Applications/Compilation.Cli/Main.cs
@@ -1,5 +1,5 @@
 using ConsoleAppFramework;
 
-using KSPCompiler.KSPCompiler.Features.Compilation.Applications.Compilation.Cli;
+using KSPCompiler.Features.Compilation.Applications.Compilation.Cli;
 
 await ConsoleApp.RunAsync( args, CompilerProgram.ExecuteCompilerAsync );
