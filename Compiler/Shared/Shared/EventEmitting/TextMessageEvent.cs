42a27743606d4fd0f0e93b1606f909d540b9f444
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2025-08-03 19:12:57 +0900

    record型に

--- Compiler/Shared/Shared/EventEmitting/TextMessageEvent.cs
+++ Compiler/Shared/Shared/EventEmitting/TextMessageEvent.cs
@@ -1,9 +1,8 @@
+using System;
+
 namespace KSPCompiler.Shared.EventEmitting;
 
-public class TextMessageEvent( string message ) : IEvent<string>
-{
-    public string Message { get; } = message;
-
-    public override string ToString()
-        => Message;
-}
+public readonly record struct TextMessageEvent(
+    string Message,
+    Exception? Error = null
+) : IEvent<string>;
