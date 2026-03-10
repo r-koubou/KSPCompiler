3b187628191bc1ac372655c8d61fa76d0492263e
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2026-03-10 13:04:53 +0900

    Scraplingを用いた実装に差し替え

--- Tools/KSPManualTools/extract_variable.py
+++ Tools/KSPManualTools/extract_variable.py
@@ -1,12 +1,9 @@
+import sys
 import os.path
-import time
 import re
 from typing import List
 
-
-import requests
-from bs4 import BeautifulSoup
-
+from scrapling.spiders import Spider, Response
 
 OUTPUT_DIR = 'output'
 OUTPUT_PREFIX = 'variables'
@@ -20,18 +17,40 @@ url_list: List[str] = [
     'https://www.native-instruments.com/ni-tech-manuals/ksp-manual/en/multi-script',
 ]
 
-def read_html(url: str) -> str:
-    """
-    Read the HTML content from a URL or file.
-    """
-    if url.startswith('http'):
-        response = requests.get(url)
-        if response.status_code != requests.codes.ok:
-            raise Exception(f"Failed to retrieve URL: {url} with status code: {response.status_code}")
-        return response.text
-    else:
-        with open(url, 'r') as file:
-            return file.read()
+class VariableSpider(Spider):
+    name = "command"
+
+    def __init__(self):
+        super().__init__()
+        self.corrected_items = []
+        self.start_urls = [
+            'https://www.native-instruments.com/ni-tech-manuals/ksp-manual/en/control-parameters',
+            'https://www.native-instruments.com/ni-tech-manuals/ksp-manual/en/engine-parameters',
+            'https://www.native-instruments.com/ni-tech-manuals/ksp-manual/en/zone-parameters',
+            'https://www.native-instruments.com/ni-tech-manuals/ksp-manual/en/multi-script',
+        ]
+
+    async def parse(self, response: Response):
+        regex_variable = re.compile(r'\s*([\$|\%|\~|\?|\@|\!][A-Z0-9_]+)')
+
+        # .code: per variable content
+        for item in response.css(".code"):
+            if item is None:
+                continue
+
+            # all variable signatures in the current item element
+            variable_signatures = item.css("code::text").getall()
+
+            if variable_signatures is None or len(variable_signatures) == 0:
+                continue
+
+            for handler in variable_signatures:
+                signature_text = str(handler).strip()
+                for x in regex_variable.findall(signature_text):
+                    self.corrected_items.append(x)
+
+            yield
+
 
 def read_previous() -> List[str]:
     """
@@ -48,34 +67,21 @@ def main(argv: List[str]) -> None:
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-    regex_command = re.compile(r'\s*([\$|\%|\~|\?|\@|\!][A-Z0-9_]+)')
     variables: List[str] = []
     previous_variables: List[str] = read_previous()
 
-    for url in url_list:
+    variable_spider = VariableSpider()
+    result = variable_spider.start()
 
-        print(f"Extracting from {url}...")
+    if result.stats.failed_requests_count > 0:
+        print(f"Failed to extract variables from {result.stats.failed_requests_count} requests.")
+        sys.exit(1)
 
-        html_text = read_html(url)
-
-        soup = BeautifulSoup(html_text, 'html.parser')
-
-        variable_elements = soup.find_all('code', {'class': 'code'})
-
-        for x in variable_elements:
-
-            variable_name: str = x.get_text(strip=True)
-            variables_names = regex_command.findall(variable_name)
-
-            if variables_names:
-                for match in variables_names:
-                    variables.append(match)
-
-        time.sleep(2)
-
+    variables = variable_spider.corrected_items
     # Remove duplicates
     # and sort the variables
     variables = list(dict.fromkeys(variables))
+    variables.sort()
     output_path = OUTPUT_PATH
 
     # Export the variables to a file
