47c5a8680009794b2d661a0eb24d08c2ca634cba
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2026-08-19 23:20:16 +0900

    YAMLデータのWebエディタ運用に伴いCLIツール削除

--- Compiler/Features/SymbolManagement/Applications/SymbolDatabaseCliApp/db_export_variables.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-setlocal
-
-set THIS_DIR=%~dp0
-
-dotnet "%THIS_DIR%\KSPCompiler.Features.SymbolManagement.Applications.SymbolDbManager.dll" export-variables %*
-
-endlocal
--- Compiler/Features/SymbolManagement/UseCase/Abstractions/SymbolManagementFailureReason.cs
+++
@@ -1,8 +0,0 @@
-namespace KSPCompiler.Features.SymbolManagement.UseCase.Abstractions;
-
-public enum SymbolManagementFailureReason
-{
-    Other,
-    IoError,
-    Cancelled,
-}
